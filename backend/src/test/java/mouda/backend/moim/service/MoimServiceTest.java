557faff6b06d98edb5e0081d9656f767f36599ee
MingyeomKim <mingyum119@naver.com>
2024-07-15 17:21:18 +0900

    fix: 데이터베이스를 초기화하여 테스트 간 데이터 격리

--- backend/src/test/java/mouda/backend/moim/service/MoimServiceTest.java
+++ backend/src/test/java/mouda/backend/moim/service/MoimServiceTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import mouda.backend.config.DatabaseCleaner;
 import mouda.backend.moim.domain.Moim;
 import mouda.backend.moim.dto.request.MoimCreateRequest;
 import mouda.backend.moim.dto.response.MoimFindAllResponses;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,14 @@ class MoimServiceTest {
 
     @Autowired
     private MoimService moimService;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @AfterEach
+    void cleanUp() {
+        databaseCleaner.cleanUp();
+    }
 
     @DisplayName("모임을 생성한다.")
     @Test
