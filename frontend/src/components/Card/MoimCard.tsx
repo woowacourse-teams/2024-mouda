86e6b9c39cf2ef19487069013fa69e7031c2af88
jaeml06 <jaeml0630@gmail.com>
2024-07-15 17:13:13 +0900

    feat: Moim card 컴포넌트 props 추가

--- frontend/src/components/Card/MoimCard.tsx
+++ frontend/src/components/Card/MoimCard.tsx
@@ -1,20 +1,27 @@
 import * as S from './MoimCard.style';
+import MoimCardProps from './MoimCard.type';
 
-export default function MoimCard() {
+export default function MoimCard(props: MoimCardProps) {
+  const {
+    data: { title, date, time, place, maxPeople },
+  } = props;
   return (
     <div css={S.cardBox}>
-      <h2 css={S.cardTitle}>축구 하실 사람?</h2>
+      <h2 css={S.cardTitle}>{title}</h2>
       <div css={S.subjectBox}>
         <span css={S.subjectTag}>날짜 및 시간</span>
-        <span css={S.subjectInfo}>7월 15일 오후 2시 </span>
+        <span css={S.subjectInfo}>
+          {date}
+          {time}
+        </span>
       </div>
       <div css={S.subjectBox}>
         <span css={S.subjectTag}>장소</span>
-        <span css={S.subjectInfo}>서울 마포구 독막로 96 1층</span>
+        <span css={S.subjectInfo}>{place}</span>
       </div>
       <div css={S.subjectBox}>
         <span css={S.subjectTag}>최대인원수</span>
-        <span css={S.subjectInfo}>최대 4명 (현재 2명)</span>
+        <span css={S.subjectInfo}>{maxPeople}</span>
       </div>
     </div>
   );
