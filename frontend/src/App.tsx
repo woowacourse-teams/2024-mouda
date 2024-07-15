b81bb0e5e046703831dadd494e63fb372ac82548
jaeml06 <jaeml0630@gmail.com>
2024-07-15 16:18:22 +0900

    feat: 모임 카드 컴포넌트 UI 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import MoimInput from './components/Input/MoimInput';
 import { Input_Info_List } from './constants';
 import { Global } from '@emotion/react';
 import reset from './reset';
+import MoimCard from './components/Card/MoimCard';
 
 export default function App() {
   return (
     <>
       <Global styles={reset} />
       <MoimInput data={Input_Info_List['title']} />
+      <MoimCard />
     </>
   );
 }
--- frontend/src/reset.ts
+++ frontend/src/reset.ts
@@ -88,6 +88,7 @@ const reset = css`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    box-sizing: border-box;
   }
   /* HTML5 display-role reset for older browsers */
   article,
