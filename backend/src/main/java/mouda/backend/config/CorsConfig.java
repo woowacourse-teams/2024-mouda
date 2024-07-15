9ae762b843aed2e3a361ad1a82d3c795c99ce965
MingyeomKim <mingyum119@naver.com>
2024-07-15 22:43:03 +0900

    fix: cors 에러 해결

--- backend/src/main/java/mouda/backend/config/CorsConfig.java
+++ backend/src/main/java/mouda/backend/config/CorsConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "OPTIONS");
 	}
 }
