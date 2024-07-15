055ae1038ac02f69b1f3fb86972e2fc40316c5b9
MingyeomKim <mingyum119@naver.com>
2024-07-15 22:50:27 +0900

    fix: CORS 설정 와일드 카드로 수정

--- backend/src/main/java/mouda/backend/config/CorsConfig.java
+++ backend/src/main/java/mouda/backend/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
-			.allowedMethods("*");
+			.allowedMethods("*")
+			.allowedHeaders("*");
 	}
 }
