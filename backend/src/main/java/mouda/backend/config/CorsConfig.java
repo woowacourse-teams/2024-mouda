e65dde232ea867292e1cccb28767b76b09b4bd34
MingyeomKim <mingyum119@naver.com>
2024-07-15 22:46:16 +0900

    fix: allowMethods를 와일드카드로 수정

--- backend/src/main/java/mouda/backend/config/CorsConfig.java
+++ backend/src/main/java/mouda/backend/config/CorsConfig.java
@@ -10,6 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
-			.allowedMethods("GET", "POST", "OPTIONS");
+			.allowedMethods("*");
 	}
 }
