d974a877eefe67899074867a203842afb44a29a6
jaeml06 <jaeml0630@gmail.com>
2024-07-15 17:23:03 +0900

    feat: MoimCardList 컴포넌트 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,45 @@ import MoimInput from './components/Input/MoimInput';
 import { Input_Info_List } from './constants';
 import { Global } from '@emotion/react';
 import reset from './reset';
-import MoimCard from './components/Card/MoimCard';
+
+import MoimCardList from './components/MoimCardList/MoimCardList';
 
 export default function App() {
+  const mockdata = [
+    {
+      title: '축구 하실 사람?',
+      place: '서울 마포구 독막로 96 1층',
+      date: '7월 15일',
+      time: '오후 2시',
+      maxPeople: 4,
+    },
+    {
+      title: '축구 하실 사람?',
+      place: '서울 마포구 독막로 96 1층',
+      date: '7월 15일',
+      time: '오후 2시',
+      maxPeople: 4,
+    },
+    {
+      title: '축구 하실 사람?',
+      place: '서울 마포구 독막로 96 1층',
+      date: '7월 15일',
+      time: '오후 2시',
+      maxPeople: 4,
+    },
+    {
+      title: '축구 하실 사람?',
+      place: '서울 마포구 독막로 96 1층',
+      date: '7월 15일',
+      time: '오후 2시',
+      maxPeople: 4,
+    },
+  ];
   return (
     <>
       <Global styles={reset} />
       <MoimInput data={Input_Info_List['title']} />
-      <MoimCard />
+      <MoimCardList data={mockdata} />
     </>
   );
 }
---
+++ frontend/src/components/MoimCardList/MoimCardList.type.ts
@@ -0,0 +1,5 @@
+import { MoimInfo } from '../../type';
+
+export default interface MoimCardListProps {
+  data: MoimInfo[];
+}
