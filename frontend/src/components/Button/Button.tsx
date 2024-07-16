d50363320ca47e076ae0653a2d56c6fa4e1b89a4
ss0526100 <ss0526100@gmail.com>
2024-07-16 13:06:01 +0900

    feat: 버튼 컴포넌트 구현 변경
    디자인, disabled 속성 추가

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -2,9 +2,9 @@ import type ButtonProps from './Button.type';
 import { shapes } from './Button.style';
 
 export default function Button(props: ButtonProps) {
-  const { shape, onClick, children } = props;
+  const { shape, onClick, disabled, children } = props;
   return (
-    <button css={shapes[shape]} onClick={onClick}>
+    <button css={shapes(shape, disabled)} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
