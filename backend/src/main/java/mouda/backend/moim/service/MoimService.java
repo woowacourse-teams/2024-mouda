da76e0aaecdb09ab84dd4e46ac0a4a931f1c3bcf
SungKyum Kim <sungkyum1@naver.com>
2024-07-16 13:38:17 +0900

    feat: 모임 삭제 API 구현

--- backend/src/main/java/mouda/backend/moim/service/MoimService.java
+++ backend/src/main/java/mouda/backend/moim/service/MoimService.java
@@ -2,7 +2,6 @@ package mouda.backend.moim.service;
 
 import java.util.List;
 
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
