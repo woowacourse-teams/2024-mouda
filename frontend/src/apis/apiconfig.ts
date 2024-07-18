ac1d56ba09e6c5ea19507e4408bee4b2d792c513
jaeml06 <jaeml0630@gmail.com>
2024-07-19 00:31:22 +0900

    feat: checkStatus 에러메세지 수정

--- frontend/src/apis/apiconfig.ts
+++ frontend/src/apis/apiconfig.ts
@@ -6,10 +6,11 @@ export const defaultOptions = {
   headers: defaultHeaders,
 };
 
-export const checkStatus = (response: Response) => {
+export const checkStatus = async (response: Response) => {
   const statusHead = Math.floor(response.status / 100);
   if (statusHead === 4 || statusHead === 5) {
-    throw new Error('요청을 처리하지 못했습니다.');
+    const json = await response.json();
+    throw new Error(json.message);
   }
   return response;
 };
