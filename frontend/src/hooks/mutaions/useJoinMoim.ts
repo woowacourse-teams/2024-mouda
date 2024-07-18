b0ae104e107d80b57cafc08551db30cfc336db21
jaeml06 <jaeml0630@gmail.com>
2024-07-18 22:31:09 +0900

    feat: 모임 참여 함수 구현

---
+++ frontend/src/hooks/mutaions/useJoinMoim.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import QUERY_KEYS from '@_constants/queryKeys';
+import { postJoinMoim } from '@_apis/posts';
+
+export default function useJoinMoim(onSuccess: () => void) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: postJoinMoim,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.moim, QUERY_KEYS.moim],
+      });
+      onSuccess();
+    },
+  });
+}
