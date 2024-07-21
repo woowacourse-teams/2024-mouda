cdd15d36053b227592494d91191e33eabd18542e
jaeml06 <jaeml0630@gmail.com>
2024-07-21 12:17:33 +0900

    chore: devserver 선언 위치 변경

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -13,6 +13,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html'),
