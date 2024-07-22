c4e8022ef0f798d2936d2cd314235a6386591c44
SungKyum Kim <sungkyum1@naver.com>
2024-07-22 14:22:02 +0900

    refactor: https 오리진 삭제

--- backend/src/main/java/mouda/backend/config/CorsConfig.java
+++ backend/src/main/java/mouda/backend/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8080", "https://localhost:8080")
+			.allowedOrigins("http://localhost:8080")
 			.allowedMethods("GET", "POST", "DELETE")
 			.allowedHeaders("Authorization", "Content-Type")
 			.allowCredentials(true)
