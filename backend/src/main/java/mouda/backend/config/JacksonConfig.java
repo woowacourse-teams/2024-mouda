db1237211d7bb8ef823f7c3be74443a8be4943b3
pricelees <priceelees@gmail.com>
2024-07-22 17:14:11 +0900

    style: 코드 컨벤션 통일

--- backend/src/main/java/mouda/backend/config/JacksonConfig.java
+++ backend/src/main/java/mouda/backend/config/JacksonConfig.java
@@ -7,7 +7,6 @@ import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
