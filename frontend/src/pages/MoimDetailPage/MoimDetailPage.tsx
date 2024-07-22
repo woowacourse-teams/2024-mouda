b7b85bab5f5bab8862277986d2f8bd6c1aee8eec
ss0526100 <ss0526100@gmail.com>
2024-07-22 20:30:49 +0900

    feat(MoimDetailPage):상세 페이지에서 참여 시 참가자 이름 추가

--- frontend/src/pages/MoimDetailPage/MoimDetailPage.tsx
+++ frontend/src/pages/MoimDetailPage/MoimDetailPage.tsx
@@ -1,18 +1,22 @@
-import useJoinMoim from '@_hooks/mutaions/useJoinMoim';
-import useMoim from '@_hooks/queries/useMoim';
-import InformationLayout from '@_layouts/InformationLayout/InformationLayout';
-import { useParams, useNavigate } from 'react-router-dom';
+import { ChangeEvent, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import BackLogo from '@_common/assets/back.svg';
-import MoimSummary from '@_components/MoimSummary/MoimSummary';
-import MoimInformation from '@_components/MoimInformation/MoimInformation';
-import MoimDescription from '@_components/MoimDescription/MoimDescription';
 import Button from '@_components/Button/Button';
+import InformationLayout from '@_layouts/InformationLayout/InformationLayout';
+import LabeledInput from '@_components/Input/MoimInput';
+import MoimDescription from '@_components/MoimDescription/MoimDescription';
+import MoimInformation from '@_components/MoimInformation/MoimInformation';
+import MoimSummary from '@_components/MoimSummary/MoimSummary';
 import ROUTES from '@_constants/routes';
+import useJoinMoim from '@_hooks/mutaions/useJoinMoim';
+import useMoim from '@_hooks/queries/useMoim';
 
 export default function MoimDetailPage() {
   const navigate = useNavigate();
   const params = useParams();
+  const [nickName, setNickName] = useState('');
+
   const moimId = Number(params.moimId);
 
   const { moim, isLoading } = useMoim(moimId);
@@ -40,9 +44,21 @@ export default function MoimDetailPage() {
         <MoimInformation moimInfo={moim} />
 
         {moim.description && <MoimDescription description={moim.description} />}
+
+        <LabeledInput
+          title="참가자 이름"
+          required
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNickName(e.target.value)
+          }
+        />
       </InformationLayout.ContentContainer>
       <InformationLayout.BottomButtonWrapper>
-        <Button shape="bar" disabled={false} onClick={() => mutate(moimId)}>
+        <Button
+          shape="bar"
+          disabled={nickName === ''}
+          onClick={() => mutate({ moimId, nickName })}
+        >
           참여하기
         </Button>
       </InformationLayout.BottomButtonWrapper>
