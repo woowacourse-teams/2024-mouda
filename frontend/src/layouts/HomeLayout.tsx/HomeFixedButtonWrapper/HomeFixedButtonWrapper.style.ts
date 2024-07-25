4b1ef564583cefd36fef285350f507fd2f9211db
cys4585 <dev.youngsu@gmail.com>
2024-07-25 11:45:51 +0900

    feature: desktop 환경에서의 max width 적용 및 상수화

--- frontend/src/layouts/HomeLayout.tsx/HomeFixedButtonWrapper/HomeFixedButtonWrapper.style.ts
+++ frontend/src/layouts/HomeLayout.tsx/HomeFixedButtonWrapper/HomeFixedButtonWrapper.style.ts
@@ -1,7 +1,14 @@
+import { DISPLAY_MAX_WIDTH } from '@_constants/styles';
 import { css } from '@emotion/react';
 
 export const fixedWrapperStyle = css`
   position: fixed;
-  right: 22px;
   bottom: 26px;
+
+  display: flex;
+  justify-content: right;
+
+  width: 100%;
+  max-width: ${DISPLAY_MAX_WIDTH};
+  padding-right: 4rem;
 `;
