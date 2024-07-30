6df542048b2a63cbd06687415af4a665935d6236
jaeml06 <jaeml0630@gmail.com>
2024-07-30 16:24:50 +0900

    style: styleLint 적용

--- frontend/src/components/Tag/Tag.style.ts
+++ frontend/src/components/Tag/Tag.style.ts
@@ -8,17 +8,20 @@ export const tagBox = (props: TagBoxProps) => {
   const { theme, color } = props;
   return css`
     ${theme.typography.tag}
+    display: inline-flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0.2rem 0.6rem;
+
     color: ${theme.colorPalette.white[100]};
+
     background-color: ${color === 'red'
       ? theme.colorPalette.red[400]
       : color === 'green'
         ? theme.colorPalette.green[200]
         : theme.colorPalette.grey[400]};
-    display: inline-flex;
-    padding: 0.2rem 0.6rem;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
     border-radius: 1rem;
   `;
 };
