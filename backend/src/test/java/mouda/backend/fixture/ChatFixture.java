e74b4df32fcd5cf6faade1ae6c99c1843b32260c
hoyeonyy <highoption@naver.com>
2024-07-31 16:44:10 +0900

    test: ChatFixture 구현

---
+++ backend/src/test/java/mouda/backend/fixture/ChatFixture.java
@@ -0,0 +1,39 @@
+package mouda.backend.fixture;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import mouda.backend.chat.domain.Chat;
+
+public class ChatFixture {
+
+	public static Chat getChatWithHogeeAtBasketballMoim() {
+		return Chat.builder()
+			.time(LocalTime.now())
+			.date(LocalDate.now())
+			.content("ㅎㅇㅎㅇ")
+			.member(MemberFixture.getHogee())
+			.moim(MoimFixture.getBasketballMoim())
+			.build();
+	}
+
+	public static Chat getChatWithAnnaAtCoffeeMoim() {
+		return Chat.builder()
+			.time(LocalTime.now())
+			.date(LocalDate.now())
+			.content("반갑읍니다")
+			.member(MemberFixture.getAnna())
+			.moim(MoimFixture.getCoffeeMoim())
+			.build();
+	}
+
+	public static Chat getChatWithTebahAtSoccerMoim() {
+		return Chat.builder()
+			.time(LocalTime.now())
+			.date(LocalDate.now())
+			.content("ㅎㅇㅎㅇ")
+			.member(MemberFixture.getTebah())
+			.moim(MoimFixture.getSoccerMoim())
+			.build();
+	}
+}
