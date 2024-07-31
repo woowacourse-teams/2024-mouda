05bf177be1d92e6337d54037da5a4722944d881b
cys4585 <dev.youngsu@gmail.com>
2024-07-31 16:29:01 +0900

    style: 기존 API 함수 원상복구

--- frontend/src/apis/gets.ts
+++ frontend/src/apis/gets.ts
@@ -2,13 +2,12 @@ import ENDPOINTS from '@_apis/endPoints';
 import { GetMoim, GetMoims } from '@_apis/responseTypes';
 import { MoimInfo } from '@_types/index';
 import { checkStatus, defaultOptions } from './apiconfig';
-import ApiClient from './apiClient';
 
 const defaultGetOptions = {
   method: 'GET',
   ...defaultOptions,
 };
-export const getMoims1 = async (): Promise<MoimInfo[]> => {
+export const getMoims = async (): Promise<MoimInfo[]> => {
   const url = ENDPOINTS.moims;
 
   const response = await fetch(url, defaultGetOptions);
@@ -17,12 +16,8 @@ export const getMoims1 = async (): Promise<MoimInfo[]> => {
   const json = (await response.json()) as GetMoims;
   return json.data.moims;
 };
-export const getMoims = async (): Promise<MoimInfo[]> => {
-  const json: GetMoims = await ApiClient.getWithAuth('moim');
-  return json.data.moims;
-};
 
-export const getMoim2 = async (moimId: number): Promise<MoimInfo> => {
+export const getMoim = async (moimId: number): Promise<MoimInfo> => {
   const url = `${ENDPOINTS.moim}/${moimId}`;
 
   const response = await fetch(url, defaultGetOptions);
@@ -30,9 +25,5 @@ export const getMoim2 = async (moimId: number): Promise<MoimInfo> => {
   checkStatus(response);
 
   const json = (await response.json()) as GetMoim;
-  return json.data;
-};
-export const getMoim = async (moimId: number): Promise<MoimInfo> => {
-  const json: GetMoim = await ApiClient.get(`moim/${moimId}`);
   return json.data;
 };
