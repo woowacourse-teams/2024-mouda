6c7807c37d12a72d6bd04823c73be9795fcc740d
pricelees <priceelees@gmail.com>
2024-07-31 16:57:04 +0900

    test: 찜 여부 조회 테스트 추가

---
+++ backend/src/test/java/mouda/backend/zzim/controller/ZzimControllerTest.java
@@ -0,0 +1,109 @@
+package mouda.backend.zzim.controller;
+
+import static org.hamcrest.Matchers.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import io.restassured.RestAssured;
+import mouda.backend.config.DatabaseCleaner;
+import mouda.backend.fixture.TokenFixture;
+import mouda.backend.member.domain.Member;
+import mouda.backend.member.repository.MemberRepository;
+import mouda.backend.moim.domain.Moim;
+import mouda.backend.moim.repository.MoimRepository;
+import mouda.backend.zzim.domain.Zzim;
+import mouda.backend.zzim.repository.ZzimRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ZzimControllerTest {
+
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private MoimRepository moimRepository;
+
+	@Autowired
+	private ZzimRepository zzimRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+	}
+
+	@Nested
+	@DisplayName("회원의 찜 여부 조회 테스트")
+	class CheckZzimByMoimAndMemberTest {
+
+		@BeforeEach
+		void setUp() {
+			databaseCleaner.cleanUp();
+		}
+
+		@DisplayName("회원이 해당 모임에 찜을 한 경우 true를 반환한다.")
+		@Test
+		void checkZzimByMoimAndMember_WhenMemberZzimed() {
+			String accessToken = TokenFixture.getTokenWithNicknameTebah();
+			Member member = memberRepository.findByNickname("테바").get();
+
+			Moim moim = moimRepository.save(Moim.builder()
+				.title("test")
+				.date(LocalDate.now().plusDays(1))
+				.time(LocalTime.now())
+				.place("test")
+				.maxPeople(10)
+				.description("test")
+				.build()
+			);
+
+			Zzim zzim = Zzim.builder().moim(moim).member(member).build();
+			zzimRepository.save(zzim);
+
+			RestAssured.given()
+				.header("Authorization", "Bearer " + accessToken)
+				.param("moimId", moim.getId())
+				.when().get("/v1/zzim/me")
+				.then().statusCode(200)
+				.body("data.isZzimed", is(true));
+		}
+
+		@DisplayName("회원이 해당 모임에 찜을 한 경우 false를 반환한다.")
+		@Test
+		void checkZzimByMoimAndMember_WhenMemberDidntZzim() {
+			String accessToken = TokenFixture.getTokenWithNicknameTebah();
+
+			Moim moim = moimRepository.save(Moim.builder()
+				.title("test")
+				.date(LocalDate.now().plusDays(1))
+				.time(LocalTime.now())
+				.place("test")
+				.maxPeople(10)
+				.description("test")
+				.build()
+			);
+
+			RestAssured.given()
+				.header("Authorization", "Bearer " + accessToken)
+				.param("moimId", moim.getId())
+				.when().get("/v1/zzim/me")
+				.then().statusCode(200)
+				.body("data.isZzimed", is(false));
+		}
+	}
+}
