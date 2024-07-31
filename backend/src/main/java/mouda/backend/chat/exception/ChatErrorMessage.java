b7a12dbc6fa07f9942f5a4492b18d5e78950fbe9
MingyeomKim <mingyum119@naver.com>
2024-07-31 15:22:48 +0900

    feat: 채팅 예외 클래스 구현

---
+++ backend/src/main/java/mouda/backend/chat/exception/ChatErrorMessage.java
@@ -0,0 +1,12 @@
+package mouda.backend.chat.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ChatErrorMessage {
+	MOIM_NOT_FOUND("존재하지 않는 모임에 채팅할 수 없습니다.");
+
+	private final String message;
+}
