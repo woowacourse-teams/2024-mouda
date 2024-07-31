8dfbbdfff5bab50a7717a1b16e17cbd76a8b9ac7
SungKyum Kim <sungkyum1@naver.com>
2024-07-31 13:07:18 +0900

    feat: 토큰 생성 클래스 구현

---
+++ backend/src/test/java/mouda/backend/security/JwtProviderTest.java
@@ -0,0 +1,35 @@
+package mouda.backend.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import mouda.backend.member.domain.Member;
+import mouda.backend.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProviderTest {
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @DisplayName("토큰을 발급한다.")
+    @Test
+    void createToken() {
+        String nickname = "테바";
+        Member member = Member.builder()
+            .nickname(nickname)
+            .build();
+        Member savedMember = memberRepository.save(member);
+        String token = jwtProvider.createToken(savedMember);
+
+        String savedNickname = jwtProvider.extractNickname(token);
+
+        assertThat(savedNickname).isEqualTo(nickname);
+    }
+}
