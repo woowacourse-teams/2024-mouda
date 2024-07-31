8a607c7f96a3ff1ab47ab78ec36df43ac9095d0f
ay-eonii <naayen@naver.com>
2024-07-31 19:30:47 +0900

    feat: 댓글 작성 일시 포맷

--- backend/src/main/java/mouda/backend/comment/dto/response/CommentResponse.java
+++ backend/src/main/java/mouda/backend/comment/dto/response/CommentResponse.java
@@ -3,15 +3,22 @@ package mouda.backend.comment.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 import mouda.backend.comment.domain.Comment;
 
 @Builder
 public record CommentResponse(
 	Long commentId,
+
 	String nickname,
+
 	String content,
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
 	LocalDateTime dateTime,
+
 	List<ChildCommentResponse> childs
 ) {
 	public static CommentResponse toResponse(Comment parentComment, List<ChildCommentResponse> childComments) {
