d588f7d449ae9816458ca5e90d6f455db4259f37
pricelees <priceelees@gmail.com>
2024-07-31 15:46:23 +0900

    feat: 테스트에서 사용하는 DatabaseCleaner에 새로 추가된 테이블 반영

--- backend/src/test/java/mouda/backend/config/DatabaseCleaner.java
+++ backend/src/test/java/mouda/backend/config/DatabaseCleaner.java
@@ -24,6 +24,18 @@ public class DatabaseCleaner {
 		entityManager.createNativeQuery("DELETE FROM MOIM").executeUpdate();
 		entityManager.createNativeQuery("ALTER TABLE MOIM alter column id restart with 1").executeUpdate();
 
+		entityManager.createNativeQuery("DELETE FROM CHAT").executeUpdate();
+		entityManager.createNativeQuery("ALTER TABLE CHAT alter column id restart with 1").executeUpdate();
+
+		entityManager.createNativeQuery("DELETE FROM COMMENT").executeUpdate();
+		entityManager.createNativeQuery("ALTER TABLE COMMENT alter column id restart with 1").executeUpdate();
+
+		entityManager.createNativeQuery("DELETE FROM ZZIM").executeUpdate();
+		entityManager.createNativeQuery("ALTER TABLE ZZIM alter column id restart with 1").executeUpdate();
+
+		entityManager.createNativeQuery("DELETE FROM CHAMYO").executeUpdate();
+		entityManager.createNativeQuery("ALTER TABLE CHAMYO alter column id restart with 1").executeUpdate();
+
 		entityManager.clear();
 	}
 }
