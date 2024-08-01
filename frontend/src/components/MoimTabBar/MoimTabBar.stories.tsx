8feff2ec29e9d78a4ee41ffe7415a35a7ea126c7
cys4585 <dev.youngsu@gmail.com>
2024-08-01 23:26:42 +0900

    test: 모임 목록 탭 수정사항 반영하여 UI Test 코드 수정

--- frontend/src/components/MoimTabBar/MoimTabBar.stories.tsx
+++ frontend/src/components/MoimTabBar/MoimTabBar.stories.tsx
@@ -11,5 +11,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    tabs: ['Tab1', 'Tab2', 'Tab3'],
+    currentTab: 'Tab1',
+    onTabClick: (tab: string) => console.log(tab),
+  },
   render: (args) => <MoimTabBar {...args} />,
 };
