469668380d7acdb029a29f20499c7a89f9ae9e7e
SungKyum Kim <sungkyum1@naver.com>
2024-08-02 15:15:13 +0900

    feat: 모임 목록에서 찜 반환 기능 구현

--- backend/src/main/java/mouda/backend/moim/dto/response/MoimFindAllResponse.java
+++ backend/src/main/java/mouda/backend/moim/dto/response/MoimFindAllResponse.java
@@ -2,33 +2,34 @@ package mouda.backend.moim.dto.response;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 import lombok.Builder;
 import mouda.backend.moim.domain.Moim;
 
 @Builder
 public record MoimFindAllResponse(
-	Long moimId,
-	String title,
-	LocalDate date,
-	LocalTime time,
-	String place,
-	int currentPeople,
-	int maxPeople,
-	String authorNickname,
-	String description
+    Long moimId,
+    String title,
+    LocalDate date,
+    LocalTime time,
+    String place,
+    int currentPeople,
+    int maxPeople,
+    String authorNickname,
+    String description,
+    boolean zzim
 ) {
 
-	public static MoimFindAllResponse toResponse(Moim moim, int currentPeople) {
-		return MoimFindAllResponse.builder()
-			.moimId(moim.getId())
-			.title(moim.getTitle())
-			.date(moim.getDate())
-			.time(moim.getTime())
-			.place(moim.getPlace())
-			.currentPeople(currentPeople)
-			.maxPeople(moim.getMaxPeople())
-			.description(moim.getDescription())
-			.build();
-	}
+    public static MoimFindAllResponse toResponse(Moim moim, int currentPeople, boolean zzim) {
+        return MoimFindAllResponse.builder()
+            .moimId(moim.getId())
+            .title(moim.getTitle())
+            .date(moim.getDate())
+            .time(moim.getTime())
+            .place(moim.getPlace())
+            .currentPeople(currentPeople)
+            .maxPeople(moim.getMaxPeople())
+            .description(moim.getDescription())
+            .zzim(zzim)
+            .build();
+    }
 }
