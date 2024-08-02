cf900a1786cd3b81b6a9f3b72a07aaf92ed33f6f
jaeml06 <jaeml0630@gmail.com>
2024-08-02 10:03:02 +0900

    feat: 모임에서의 내 권한을 get 함수

--- frontend/src/apis/gets.ts
+++ frontend/src/apis/gets.ts
@@ -2,6 +2,7 @@
 import { MoimInfo } from '@_types/index';
 import ApiClient from './apiClient';
 import { GetMoim, GetMoims } from './responseTypes';
+import { GetChamyoMine, GetMoim, GetMoims, GetZzimMine } from './responseTypes';
 import { checkStatus } from './apiconfig';
 
 export const getMoims = async (): Promise<MoimInfo[]> => {
@@ -19,3 +20,14 @@ export const getMoim = async (moimId: number): Promise<MoimInfo> => {
   const json: GetMoim = await response.json();
   return json.data;
 };
+
+export const getChamyoMine = async (
+  moimId: number,
+): Promise<'MOIMER' | 'MOIMEE' | 'NON_MOIMEE'> => {
+  const response = await ApiClient.getWithAuth(`chamyo/mine?moimId=${moimId}`);
+  checkStatus(response);
+
+  const json: GetChamyoMine = await response.json();
+  return json.data.role;
+};
+
