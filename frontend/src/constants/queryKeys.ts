04611d05577508dc510efa42c5361509c6fc735d
jaeml06 <jaeml0630@gmail.com>
2024-08-02 10:07:38 +0900

    feat: queryKey 추가

--- frontend/src/constants/queryKeys.ts
+++ frontend/src/constants/queryKeys.ts
@@ -1,3 +1,10 @@
-const QUERY_KEYS = { moims: 'moims', moim: 'moim', userKey: 'userKey' };
+const QUERY_KEYS = {
+  moims: 'moims',
+  moim: 'moim',
+  userKey: 'userKey',
+  chamyoMine: 'chamyoMine',
+  ZzimMine: 'ZzimMine',
+  comment: 'comment',
+};
 
 export default QUERY_KEYS;
