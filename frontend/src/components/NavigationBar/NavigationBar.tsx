26947c95ffcbf05f50536a3d6e0cd9c9ff3e6dea
cys4585 <dev.youngsu@gmail.com>
2024-08-03 21:03:17 +0900

    feature: 필요한 페이지에 하단 네비게이션 바를 적용

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -2,26 +2,41 @@ import * as S from './NavigationBar.style';
 import { useState } from 'react';
 import NavigationBarItem from '@_components/NavigationBarItem/NavigationBarItem';
 import { useTheme } from '@emotion/react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import ROUTES from '@_constants/routes';
 
 export type Tab = '홈' | '채팅' | '해주세요' | '마이페이지';
-const tabs: Tab[] = ['홈', '채팅', '해주세요', '마이페이지'];
+
+const tabRoutes: Record<Tab, string> = {
+  홈: ROUTES.main,
+  채팅: ROUTES.chat,
+  해주세요: ROUTES.please,
+  마이페이지: ROUTES.myPage,
+};
 
 export default function NavigationBar() {
   const theme = useTheme();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const [currentTab, setCurrentTab] = useState<Tab>('홈');
+  const [currentTab, setCurrentTab] = useState<Tab>(
+    Object.keys(tabRoutes).find(
+      (key) => tabRoutes[key as Tab] === location.pathname,
+    ) as Tab,
+  );
 
   const handleTabClick = (tab: Tab) => {
     setCurrentTab(tab);
+    navigate(tabRoutes[tab]);
   };
 
   return (
     <nav css={S.navigationBarContainer({ theme })}>
       <ul css={S.navigationBarList}>
-        {tabs.map((tab) => (
+        {Object.keys(tabRoutes).map((tab) => (
           <NavigationBarItem
             key={tab}
-            tab={tab}
+            tab={tab as Tab}
             isActive={currentTab === tab}
             onClick={handleTabClick}
           />
