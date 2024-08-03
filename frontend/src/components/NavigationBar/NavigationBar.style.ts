c3ea7c5b3a63061c6a7087a8e39afd69ce1ac2ad
cys4585 <dev.youngsu@gmail.com>
2024-08-03 13:30:11 +0900

    feature: 네비게이션 바 typography, color 적용, 활성화 기능 적용

--- frontend/src/components/NavigationBar/NavigationBar.style.ts
+++ frontend/src/components/NavigationBar/NavigationBar.style.ts
@@ -20,18 +20,3 @@ export const navigationBarList = css`
 
   list-style: none;
 `;
-
-export const navigationBarItem = css`
-  display: flex;
-  flex: 1; /* 각 아이템이 동일한 너비를 가지도록 설정 */
-  flex-direction: column;
-  gap: 4px;
-  align-items: center;
-  justify-content: center;
-
-  color: #6d717f;
-
-  :hover {
-    color: #000;
-  }
-`;
