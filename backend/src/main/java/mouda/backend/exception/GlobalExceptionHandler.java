6ebb8c38640c783be3b48d64f7493a6111a7b2ac
MingyeomKim <mingyum119@naver.com>
2024-08-04 21:57:09 +0900

    feat: `@RequestParameter`에 NULL이 입력되었을 때 예외 핸들링

--- backend/src/main/java/mouda/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/mouda/backend/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -13,21 +14,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-        HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        String error = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+	@Override
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(
+		MissingServletRequestParameterException exception,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request
+	) {
+		return ResponseEntity.badRequest().body(new ErrorResponse(exception.getParameterName() + "은 NULL일 수 없습니다."));
+	}
 
-        return ResponseEntity.badRequest().body(new ErrorResponse(error));
-    }
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		String error = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
 
-    @ExceptionHandler(MoudaException.class)
-    public ResponseEntity<ErrorResponse> handleMoudaException(MoudaException exception) {
-        return ResponseEntity.status(exception.getHttpStatus()).body(new ErrorResponse(exception.getMessage()));
-    }
+		return ResponseEntity.badRequest().body(new ErrorResponse(error));
+	}
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException() {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("서버 오류가 발생했습니다."));
-    }
+	@ExceptionHandler(MoudaException.class)
+	public ResponseEntity<ErrorResponse> handleMoudaException(MoudaException exception) {
+		return ResponseEntity.status(exception.getHttpStatus()).body(new ErrorResponse(exception.getMessage()));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException() {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("서버 오류가 발생했습니다."));
+	}
 }
