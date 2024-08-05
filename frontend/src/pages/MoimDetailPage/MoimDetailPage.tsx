3fce1e2ddec440b4f7720343c9a8b788463c8a84
jaeml06 <jaeml0630@gmail.com>
2024-08-05 20:34:09 +0900

    fix: 모임 수정이 불가되는 에러 해결

--- frontend/src/pages/MoimDetailPage/MoimDetailPage.tsx
+++ frontend/src/pages/MoimDetailPage/MoimDetailPage.tsx
@@ -72,6 +72,7 @@ export default function MoimDetailPage() {
                     navigate(`/modify/${moimId}`, {
                       state: {
                         ...moim,
+                        moimId,
                       },
                     }),
                 },
