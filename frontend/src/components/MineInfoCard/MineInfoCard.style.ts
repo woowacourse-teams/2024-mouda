e3d366d707d2ec4fff1635380a03469eb173d151
jaeml06 <jaeml0630@gmail.com>
2024-08-06 15:18:39 +0900

    feat: MinInfoCard 컴포넌트 UI 구현

---
+++ frontend/src/components/MineInfoCard/MineInfoCard.style.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+export const MineInfoContainer = () => css`
+  display: flex;
+  gap: 1em;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+`;
+export const MinetextWrapper = () => css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: flex-end;
+`;
---
+++ frontend/src/components/MineInfoCard/MineInfoCard.tsx
@@ -0,0 +1,22 @@
+import * as S from '@_components/MineInfoCard/MineInfoCard.style';
+import ProfileFrame from '@_components/Profile/ProfileFrame';
+import { useTheme } from '@emotion/react';
+
+interface MineInfoCardProps {
+  nickname: string;
+  profile: string;
+}
+export default function MineInfoCard(props: MineInfoCardProps) {
+  const { nickname, profile } = props;
+  const theme = useTheme();
+  return (
+    <div css={S.MineInfoContainer()}>
+      <ProfileFrame width={9} height={9} src={profile}></ProfileFrame>
+      <div css={S.MinetextWrapper}>
+        <span css={theme.typography.s1}></span>
+        <span css={theme.typography.h4}>{nickname}</span>
+        <span css={theme.typography.c1}>우아한테크코스 6기</span>
+      </div>
+    </div>
+  );
+}
