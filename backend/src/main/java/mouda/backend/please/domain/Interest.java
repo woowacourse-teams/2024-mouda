011adb6cb429abbc91ed8027d78ff9ad4850cdf5
ay-eonii <naayen@naver.com>
2024-08-06 14:55:20 +0900

    feat: 관심 도메인

---
+++ backend/src/main/java/mouda/backend/please/domain/Interest.java
@@ -0,0 +1,31 @@
+package mouda.backend.please.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mouda.backend.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Interest {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Member member;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Please please;
+}
+
+
