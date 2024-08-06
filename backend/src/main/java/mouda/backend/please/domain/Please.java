34ca477a51805cff36376781044ad8da8ac758e3
ay-eonii <naayen@naver.com>
2024-08-06 14:55:14 +0900

    feat: 해주세요 도메인

---
+++ backend/src/main/java/mouda/backend/please/domain/Please.java
@@ -0,0 +1,36 @@
+package mouda.backend.please.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Please {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private long authorId;
+
+	@Builder
+	public Please(String title, String description, long authorId) {
+		this.title = title;
+		this.description = description;
+		this.authorId = authorId;
+	}
+}
