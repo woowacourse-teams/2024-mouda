5374d88c4fa4f62c985ecaffbd3693d9d7521f75
SungKyum Kim <sungkyum1@naver.com>
2024-08-07 16:04:43 +0900

    refactor: 핸들러 로직 개선

--- backend/src/main/java/mouda/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/mouda/backend/exception/GlobalExceptionHandler.java
@@ -1,30 +1,22 @@
 package mouda.backend.exception;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
-	@Override
-	protected ResponseEntity<Object> handleMissingServletRequestParameter(
-		MissingServletRequestParameterException exception,
-		HttpHeaders headers, HttpStatusCode status, WebRequest request
-	) {
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<ErrorResponse> handleMissingServletRequestParameter(
+		MissingServletRequestParameterException exception) {
 		return ResponseEntity.badRequest().body(new ErrorResponse(exception.getParameterName() + "은 NULL일 수 없습니다."));
 	}
 
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
 		String error = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
 
 		return ResponseEntity.badRequest().body(new ErrorResponse(error));
@@ -38,6 +30,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception exception) {
 		exception.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("서버 오류가 발생했습니다."));
+		return ResponseEntity.internalServerError().body(new ErrorResponse("서버 오류가 발생했습니다."));
 	}
 }
