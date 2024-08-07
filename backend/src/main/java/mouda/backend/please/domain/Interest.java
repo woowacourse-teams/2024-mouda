ace996351f324cd1fd096f28b64d89e9f4427a83
SungKyum Kim <sungkyum1@naver.com>
2024-08-07 13:36:02 +0900

    feat: 멤버, 해주세요 검증 로직 추가

--- backend/src/main/java/mouda/backend/please/domain/Interest.java
+++ backend/src/main/java/mouda/backend/please/domain/Interest.java
@@ -1,5 +1,7 @@
 package mouda.backend.please.domain;
 
+import org.springframework.http.HttpStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +12,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import mouda.backend.member.domain.Member;
+import mouda.backend.please.exception.PleaseErrorMessage;
+import mouda.backend.please.exception.PleaseException;
 
 @Entity
 @Getter
@@ -30,7 +34,21 @@ public class Interest {
 
 	@Builder
 	public Interest(Member member, Please please) {
+		validateMember(member);
+		validatePlease(please);
 		this.member = member;
 		this.please = please;
+	}
+
+	private void validateMember(Member member) {
+		if (member == null) {
+			throw new PleaseException(HttpStatus.NOT_FOUND, PleaseErrorMessage.MEMBER_NOT_FOUND);
+		}
+	}
+
+	private void validatePlease(Please please) {
+		if (please == null) {
+			throw new PleaseException(HttpStatus.NOT_FOUND, PleaseErrorMessage.NOT_FOUND);
+		}
 	}
 }
