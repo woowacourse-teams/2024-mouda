4989775af09736b30be22dd75f2f4ca32ba42ca6
cys4585 <dev.youngsu@gmail.com>
2024-08-08 12:48:42 +0900

    fix: GA 초기화 중복 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,7 @@
 import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactGA from 'react-ga4';
 
-// 구글 애널리틱스 운영서버만 적용
-if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
-  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
-}
 async function enableMocking() {
   if (process.env.MSW !== 'true') {
     return;
