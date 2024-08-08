0ca92856b87c12132abb30a025e39161864aae3f
ss0526100 <ss0526100@gmail.com>
2024-08-09 00:18:27 +0900

    feat:missingFallback 구현

--- frontend/src/components/MissingFallback/MissingFallback.style.ts
+++ frontend/src/components/MissingFallback/MissingFallback.style.ts
@@ -2,13 +2,12 @@ import { css } from '@emotion/react';
 
 export const container = css`
   display: flex;
-`;
-
-export const image = css`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+`;
 
+export const image = css`
   width: 50%;
   max-width: 30rem;
   height: 50%;
