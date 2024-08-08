96552a973ceec6dbf7d82fe7ee024b7b7c7b2d19
ss0526100 <ss0526100@gmail.com>
2024-08-08 20:29:38 +0900

    test(moimHandler): 안쓰는 쿼리 삭제

--- frontend/src/mocks/handler/moimHandler.ts
+++ frontend/src/mocks/handler/moimHandler.ts
@@ -215,7 +215,7 @@ export const moimHandler = [
       },
     });
   }),
-  http.get(`${ENDPOINTS.chamyo}/all?moimId=1`, () => {
+  http.get(`${ENDPOINTS.chamyo}/all`, () => {
     return HttpResponse.json({
       data: {
         chamyos: [
@@ -238,14 +238,14 @@ export const moimHandler = [
       },
     });
   }),
-  http.get(`${ENDPOINTS.zzim}/mine?moimId=1`, () => {
+  http.get(`${ENDPOINTS.zzim}/mine`, () => {
     return HttpResponse.json({
       data: {
         isZzimed: false,
       },
     });
   }),
-  http.get(`${ENDPOINTS.chamyo}/mine?moimId=1`, () => {
+  http.get(`${ENDPOINTS.chamyo}/mine`, () => {
     return HttpResponse.json({
       data: {
         role: 'MOIMER',
