971db05d8b562acbe64c0db330c4e8c175cff3db
cys4585 <dev.youngsu@gmail.com>
2024-08-08 13:48:56 +0900

    sentry 에러 트래킹 테스트

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,24 @@ import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+//...
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', 'http://43.202.67.25/'],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 async function enableMocking() {
   if (process.env.MSW !== 'true') {
     return;
