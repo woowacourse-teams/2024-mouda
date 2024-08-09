bedb8373da3b10a267a5b1feb096e5ceb3c7bbe1
ss0526100 <ss0526100@gmail.com>
2024-08-09 13:25:14 +0900

    fix: 채팅방 띄어쓰기 안되는 버그 수정

--- frontend/src/components/ChatBubble/ChatBubble.style.ts
+++ frontend/src/components/ChatBubble/ChatBubble.style.ts
@@ -16,6 +16,7 @@ export const chatBubble = ({
   padding: 10px;
 
   word-break: break-all;
+  white-space: pre;
 
   background-color: ${backgroundColor || theme.semantic.primary};
   border-radius: ${isMyMessage ? '12px' : 0} ${isMyMessage ? 0 : '12px'} 12px
--- frontend/src/components/ChatBubble/ChatChildren/ChatChildren.style.tsx
+++ frontend/src/components/ChatBubble/ChatChildren/ChatChildren.style.tsx
@@ -4,3 +4,7 @@ export const grey400C2 = ({ theme }: { theme: Theme }) => css`
   ${theme.typography.c2}
   color:${theme.colorPalette.grey[400]}
 `;
+
+export const basicChat = css`
+  white-space: pre;
+`;
