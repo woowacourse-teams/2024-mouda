24dc3313ef78f23f0842a65d2d1839961de83f42
jaeml06 <jaeml0630@gmail.com>
2024-08-09 12:17:27 +0900

    fix: 모임 참여자 목록 쿼리 무효화

--- frontend/src/hooks/mutaions/useCancelChamyo.ts
+++ frontend/src/hooks/mutaions/useCancelChamyo.ts
@@ -13,6 +13,9 @@ export default function useCancelChamyo() {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.chamyoMine],
       });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.chamyoAll],
+      });
     },
   });
 }
