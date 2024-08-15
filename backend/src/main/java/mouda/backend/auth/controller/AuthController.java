cae2b7993391a1b6f95d593162e5059527a8d73c
hoyeonyy <highoption@naver.com>
2024-08-15 14:21:35 +0900

    feat: 인가 코드 받아오는 메서드 생성

--- backend/src/main/java/mouda/backend/auth/controller/AuthController.java
+++ backend/src/main/java/mouda/backend/auth/controller/AuthController.java
@@ -1,28 +1,46 @@
 package mouda.backend.auth.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.RequiredArgsConstructor;
 import mouda.backend.auth.dto.request.LoginRequest;
 import mouda.backend.auth.dto.response.LoginResponse;
 import mouda.backend.auth.service.AuthService;
 import mouda.backend.common.RestResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import mouda.backend.member.domain.Member;
+import mouda.backend.member.repository.MemberRepository;
 
 @RestController
 @RequestMapping("/v1/auth")
 @RequiredArgsConstructor
 public class AuthController implements AuthSwagger {
 
-    private final AuthService authService;
+	private final AuthService authService;
+	private final MemberRepository memberRepository;
 
-    @Override
-    @PostMapping("/login")
-    public ResponseEntity<RestResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
-        LoginResponse response = authService.login(loginRequest);
+	@Override
+	@PostMapping("/login")
+	public ResponseEntity<RestResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
+		LoginResponse response = authService.login(loginRequest);
 
-        return ResponseEntity.ok().body(new RestResponse<>(response));
-    }
+		return ResponseEntity.ok().body(new RestResponse<>(response));
+	}
+
+	@GetMapping("/kakao/oauth")
+	public void kakao(
+		@RequestParam("code") String code,
+		@RequestParam("error") String error,
+		@RequestParam("error_description") String error_description,
+		@RequestParam("state") String state
+	) {
+		System.out.println(code);
+		Member member = new Member(code);
+		memberRepository.save(member);
+	}
 }
