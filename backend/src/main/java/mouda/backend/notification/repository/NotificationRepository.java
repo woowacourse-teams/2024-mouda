c6767fa9b0fa14001e86e8c18431af6e9c198cf6
pricelees <priceelees@gmail.com>
2024-08-15 19:28:48 +0900

    remove: 미사용 클래스 제거

--- backend/src/main/java/mouda/backend/notification/repository/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package mouda.backend.notification.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mouda.backend.notification.domain.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
