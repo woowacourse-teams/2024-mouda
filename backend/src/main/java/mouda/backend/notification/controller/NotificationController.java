7c86fbfa1ae6259257cd2062e91e6459bac739c6
pricelees <priceelees@gmail.com>
2024-08-15 19:32:57 +0900

    feat: FCM 토큰과 회원을 등록하는 API 구현

--- backend/src/main/java/mouda/backend/notification/controller/NotificationController.java
+++ backend/src/main/java/mouda/backend/notification/controller/NotificationController.java
@@ -1,33 +1,33 @@
 package mouda.backend.notification.controller;
 
-import java.io.IOException;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mouda.backend.config.argumentresolver.LoginMember;
 import mouda.backend.member.domain.Member;
-import mouda.backend.notification.dto.request.FcmSendRequest;
 import mouda.backend.notification.dto.request.FcmTokenSaveRequest;
 import mouda.backend.notification.service.NotificationService;
 
 @RestController
 @RequestMapping("/v1/notification")
 @RequiredArgsConstructor
-public class NotificationController {
+public class NotificationController implements NotificationSwagger {
 
 	private final NotificationService notificationService;
 
-	@PostMapping("/send")
-	public void saveFcmToken(@LoginMember Member member, @RequestBody FcmTokenSaveRequest fcmTokenSaveRequest) {
-		notificationService.saveFcmToken(member.getId(), fcmTokenSaveRequest);
-	}
+	@Override
+	@PostMapping("/register")
+	public ResponseEntity<Void> registerFcmToken(
+		@LoginMember Member member,
+		@Valid @RequestBody FcmTokenSaveRequest fcmTokenSaveRequest
+	) {
+		notificationService.registerFcmToken(member.getId(), fcmTokenSaveRequest);
 
-	@PostMapping("/message")
-	public void test(@RequestBody FcmSendRequest fcmSendRequest) throws IOException {
-		notificationService.sendMessage(fcmSendRequest);
+		return ResponseEntity.ok().build();
 	}
 }
