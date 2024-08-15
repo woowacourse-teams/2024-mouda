8a8d2ed7d5527c0e2699c97a0295f83e01041531
cys4585 <dev.youngsu@gmail.com>
2024-08-15 22:13:29 +0900

    feature: 모임장소 스탭 UI 구현

--- frontend/src/pages/MoimCreationPage/Steps/PlaceStep.tsx
+++ frontend/src/pages/MoimCreationPage/Steps/PlaceStep.tsx
@@ -1,4 +1,18 @@
 import FunnelQuestion from '@_components/Funnel/FunnelQuestion/FunnelQuestion';
+import FunnelRadioCardGroup from '@_components/Funnel/FunnelRadioCardGroup/FunnelRadioCardGroup';
+
+const options = [
+  {
+    title: '오프라인',
+    description: '농구, 간단한 맥주, 커피챗 등 집 밖에서 친구들을 만나보세요!',
+    isSelected: true,
+  },
+  {
+    title: '온라인',
+    description: '칼바람 나락, 디스코드, 줌 등 온라인으로 친구들을 만나보세요',
+    isSelected: false,
+  },
+];
 
 export default function PlaceStep() {
   return (
@@ -9,6 +23,17 @@ export default function PlaceStep() {
         <br />
         <FunnelQuestion.Text>모이나요?</FunnelQuestion.Text>
       </FunnelQuestion>
+      <FunnelRadioCardGroup>
+        {options.map((option) => (
+          <FunnelRadioCardGroup.Option
+            key={option.title}
+            title={option.title}
+            description={option.description}
+            isSelected={option.isSelected}
+            onSelect={() => {}}
+          />
+        ))}
+      </FunnelRadioCardGroup>
     </>
   );
 }
