aa7994573e6cb88158b1927249d0d21dbd3fe59f
ay-eonii <naayen@naver.com>
2024-08-16 15:49:32 +0900

    test: 다락방 멤버 권한 조회

--- backend/src/test/java/mouda/backend/darakbangmember/service/DarakbangMemberServiceTest.java
+++ backend/src/test/java/mouda/backend/darakbangmember/service/DarakbangMemberServiceTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import mouda.backend.config.DatabaseCleaner;
 import mouda.backend.darakbang.domain.Darakbang;
 import mouda.backend.darakbang.repository.DarakbangRepository;
+import mouda.backend.darakbangmember.domain.DarakBangMemberRole;
+import mouda.backend.darakbangmember.dto.response.DarakbangMemberRoleResponse;
 import mouda.backend.darakbangmember.exception.DarakbangMemberException;
 import mouda.backend.darakbangmember.repository.repository.DarakbangMemberRepository;
 import mouda.backend.fixture.DarakbangFixture;
@@ -56,6 +58,23 @@ class DarakbangMemberServiceTest {
 
 			assertThatThrownBy(() -> darakbangMemberService.findAllDarakbangMembers(darakbang.getId(), hogee))
 				.isInstanceOf(DarakbangMemberException.class);
+		}
+	}
+
+	@DisplayName("다락방 멤버 권한 조회 테스트")
+	@Nested
+	class DarakbangMemberRoleReadTest {
+
+		@DisplayName("다락방 멤버가 아니라면 OUTSIDER를 반환한다.")
+		@Test
+		void success() {
+			Darakbang darakbang = darakbangRepository.save(DarakbangFixture.getDarakbangWithWooteco());
+			Member hogee = memberRepository.save(MemberFixture.getHogee());
+
+			DarakbangMemberRoleResponse response = darakbangMemberService.findDarakbangMemberRole(
+				darakbang.getId(), hogee);
+
+			assertThat(response.role()).isEqualTo(DarakBangMemberRole.OUTSIDER);
 		}
 	}
 }
