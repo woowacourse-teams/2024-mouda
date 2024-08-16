1f0a3afffe79f17958ae4cb314c0b8494c1e53c1
SungKyum Kim <sungkyum1@naver.com>
2024-08-16 14:55:16 +0900

    feat: readTimeout ConnectionTimeout 검증 추가

--- backend/src/main/java/mouda/backend/config/RestClientConfig.java
+++ backend/src/main/java/mouda/backend/config/RestClientConfig.java
@@ -2,6 +2,7 @@ package mouda.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -10,7 +11,15 @@ public class RestClientConfig {
 	@Bean
 	public RestClient kakaoOauthRestClient() {
 		return RestClient.builder()
+			.requestFactory(getClientHttpRequestFactory())
 			.baseUrl("https://kauth.kakao.com")
 			.build();
+	}
+
+	private HttpComponentsClientHttpRequestFactory getClientHttpRequestFactory() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(100);
+		clientHttpRequestFactory.setConnectionRequestTimeout(70);
+		return clientHttpRequestFactory;
 	}
 }
