c49c15d6ad3003637b8bc37679cc4ff84d692f89
ss0526100 <ss0526100@gmail.com>
2024-08-18 00:26:14 +0900

    feat: 다락방 코드를 확인하는 중간 루트 페이지 구현

--- frontend/src/routes/DarakbangInvitationRoute.tsx
+++ frontend/src/routes/DarakbangInvitationRoute.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import Button from '@_components/Button/Button';
+import CompleteLayout from '@_layouts/CompleteLayout/CompleteLayout';
 import MissingFallback from '@_components/MissingFallback/MissingFallback';
 import ROUTES from '@_constants/routes';
 
@@ -11,18 +12,23 @@ export default function DarakbangInvitationRoute() {
   const code = searchParam.get('code');
   const isRightCode = code === '1';
   const darakbangName = '우아한테크코스';
-  if (isRightCode)
+  if (isRightCode) {
     navigate(ROUTES.darakbangNickname, { state: { darakbangName } });
+  }
 
   return (
-    <>
-      <MissingFallback text="유효하지 않은 참여코드입니다." />
-      <Button
-        shape="bar"
-        onClick={() => navigate(ROUTES.darakbangSelectOption)}
-      >
-        참여페이지로 돌아가기
-      </Button>
-    </>
+    <CompleteLayout>
+      <CompleteLayout.ContentContainer>
+        <MissingFallback text="유효하지 않은 참여코드입니다" />
+      </CompleteLayout.ContentContainer>
+      <CompleteLayout.BottomButtonWrapper>
+        <Button
+          shape="bar"
+          onClick={() => navigate(ROUTES.darakbangSelectOption)}
+        >
+          참여페이지로 돌아가기
+        </Button>
+      </CompleteLayout.BottomButtonWrapper>
+    </CompleteLayout>
   );
 }
