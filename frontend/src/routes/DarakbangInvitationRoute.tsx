f262d5650a9bc80c3a9200a473fee5426329153f
ss0526100 <ss0526100@gmail.com>
2024-08-18 00:18:00 +0900

    feat: 다락방 코드를 확인하는 중간 루트 페이지 구현

---
+++ frontend/src/routes/DarakbangInvitationRoute.tsx
@@ -0,0 +1,28 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import Button from '@_components/Button/Button';
+import MissingFallback from '@_components/MissingFallback/MissingFallback';
+import ROUTES from '@_constants/routes';
+
+export default function DarakbangInvitationRoute() {
+  const [searchParam] = useSearchParams();
+  const navigate = useNavigate();
+
+  const code = searchParam.get('code');
+  const isRightCode = code === '1';
+  const darakbangName = '우아한테크코스';
+  if (isRightCode)
+    navigate(ROUTES.darakbangNickname, { state: { darakbangName } });
+
+  return (
+    <>
+      <MissingFallback text="유효하지 않은 참여코드입니다." />
+      <Button
+        shape="bar"
+        onClick={() => navigate(ROUTES.darakbangSelectOption)}
+      >
+        참여페이지로 돌아가기
+      </Button>
+    </>
+  );
+}
