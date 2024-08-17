ce2fc39efdad2a91c2e1d69a21a6e3f8e0bbebaf
ss0526100 <ss0526100@gmail.com>
2024-08-18 03:17:49 +0900

    feat(DarakbangInvitationPage): 초대링크 공유하기

--- frontend/src/pages/DarakbangInvitationPage/DarakbangInvitationPage.tsx
+++ frontend/src/pages/DarakbangInvitationPage/DarakbangInvitationPage.tsx
@@ -2,6 +2,7 @@ import * as S from './DarakbangInvitationPage.style';
 
 import Button from '@_components/Button/Button';
 import CompleteBottomWrapper from '@_layouts/CompleteLayout/CompleteBottomWrapper/CompleteBottomWrapper';
+import ROUTES from '@_constants/routes';
 import SolidArrow from '@_components/Icons/SolidArrow';
 import StickyTriSectionHeader from '@_layouts/components/StickyTriSectionHeader/StickyTriSectionHeader';
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +12,7 @@ export default function DarakbangInvitationPage() {
   const theme = useTheme();
   const navigate = useNavigate();
   const code = 'NUFSOU9';
+  const darakbangName = '우아한테크코스';
   return (
     <div css={S.layout}>
       <StickyTriSectionHeader>
@@ -25,8 +27,30 @@ export default function DarakbangInvitationPage() {
         <div css={S.code({ theme })}>{code}</div>
 
         <CompleteBottomWrapper>
-          <Button shape="bar" primary>
-            초대링크 복사하기
+          <Button
+            shape="bar"
+            primary
+            onClick={async () => {
+              const url = `${process.env.BASE_URL}${ROUTES.darakbangInvitationRoute}?code=${code}`;
+              if (navigator.share) {
+                await navigator.share({
+                  title: `${darakbangName} 다락방으로의 초대`,
+                  text: `${darakbangName}으로 초대되었어요~!\n모우다에서 함께 모여보세요~`,
+                  url,
+                });
+                return;
+              }
+
+              if (document.location.protocol === 'https:') {
+                await navigator.clipboard.writeText(
+                  `${process.env.BASE_URL}${1}?code=${1}`,
+                );
+                return;
+              }
+              alert('http를 통해서는 복사할 수 없습니다!');
+            }}
+          >
+            초대링크 공유하기
           </Button>
         </CompleteBottomWrapper>
       </div>
