3ae3b1a1673491bc5f3ac2fa46ff575522aaf931
cys4585 <dev.youngsu@gmail.com>
2024-08-17 19:41:29 +0900

    refactor: react 상태와 storage 상태 동기화 보장하도록 setter 함수화, unmount 시 스토리지 데이터 삭제 옵션 부여

--- frontend/src/hooks/useStatePersist.ts
+++ frontend/src/hooks/useStatePersist.ts
@@ -6,12 +6,14 @@ interface UseStatePersistParams<StateType> {
   key: string;
   initialState: StateType | (() => StateType);
   storage?: StorageType;
+  removeOnUnmount?: boolean;
 }
 
 export default function useStatePersist<StateType>({
   key,
   initialState,
   storage = 'sessionStorage',
+  removeOnUnmount = true,
 }: UseStatePersistParams<StateType>): [
   StateType,
   React.Dispatch<React.SetStateAction<StateType>>,
@@ -34,25 +36,39 @@ export default function useStatePersist<StateType>({
       : initialState;
   };
 
-  const [state, setState] = useState<StateType>(getStoredValue);
-
-  useEffect(() => {
+  const setStoredValue = (value: StateType) => {
     try {
-      storageObject.setItem(key, JSON.stringify(state));
+      storageObject.setItem(key, JSON.stringify(value));
     } catch (error) {
       console.error(`Error writing to ${storage}`, error);
     }
-  }, [key, state, storageObject, storage]);
+  };
+
+  const [state, setState] = useState<StateType>(getStoredValue);
+
+  const setStatePersist = (
+    newState: StateType | ((prevState: StateType) => StateType),
+  ) => {
+    const updatedState =
+      typeof newState === 'function'
+        ? (newState as (prevState: StateType) => StateType)(state)
+        : newState;
+
+    setStoredValue(updatedState);
+    setState(updatedState);
+  };
 
   useEffect(() => {
     return () => {
-      try {
-        storageObject.removeItem(key);
-      } catch (error) {
-        console.error(`Error removing item from ${storage}`, error);
+      if (removeOnUnmount) {
+        try {
+          storageObject.removeItem(key);
+        } catch (error) {
+          console.error(`Error removing item from ${storage}`, error);
+        }
       }
     };
-  }, [key, storageObject, storage]);
+  }, []);
 
-  return [state, setState];
+  return [state, setStatePersist];
 }
