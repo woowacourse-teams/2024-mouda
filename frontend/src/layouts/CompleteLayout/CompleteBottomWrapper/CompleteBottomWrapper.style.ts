69a7d4d4eec5d19a7ab20c55ad74fa3a1ac59b57
ss0526100 <ss0526100@gmail.com>
2024-08-19 01:38:08 +0900

    fix(CompleteBottomButtonWrapper): 화면을 벗어나는 버그 수정

--- frontend/src/layouts/CompleteLayout/CompleteBottomWrapper/CompleteBottomWrapper.style.ts
+++ frontend/src/layouts/CompleteLayout/CompleteBottomWrapper/CompleteBottomWrapper.style.ts
@@ -1,10 +1,9 @@
-import { DISPLAY_MAX_WIDTH } from '@_constants/styles';
 import { css } from '@emotion/react';
 
 // TODO: 바텀 버튼 UI에 대한 기획 논의 필요
 export const bottomFixedStyle = css`
   position: fixed;
   bottom: 26px;
-  width: ${DISPLAY_MAX_WIDTH};
+  width: 100%;
   padding: 0 16px;
 `;
