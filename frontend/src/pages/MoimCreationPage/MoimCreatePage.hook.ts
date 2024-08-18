7135af1772d5666abbee2b92402dcd53e009dddf
cys4585 <dev.youngsu@gmail.com>
2024-08-19 00:51:41 +0900

    feature: 링크 클릭, URL 직접 입력으로 모임 만들기 페이지에 들어온 경우 스토리지 remove하도록 대응

--- frontend/src/pages/MoimCreationPage/MoimCreatePage.hook.ts
+++ frontend/src/pages/MoimCreationPage/MoimCreatePage.hook.ts
@@ -16,13 +16,39 @@ type MoimCreationInputInfo = MoimInputInfo & {
   description: string;
 };
 
+/**
+ * @description 브라우저의 네비게이션 타입을 구분하는 함수.
+ * 링크 클릭, URL 직접 입력으로 페이지에 들어온 경우를 구분하기 위해 사용.
+ * react router로는 감지할 수 없어서 사용.
+ * ref: https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigationTiming/type
+ */
+const getBrowserNavigationType = ():
+  | 'navigate'
+  | 'reload'
+  | 'back_forward'
+  | 'prerender'
+  | undefined => {
+  const navEntries = performance.getEntriesByType('navigation');
+  if (navEntries.length > 0) {
+    const navEntry = navEntries[0] as PerformanceNavigationTiming;
+    return navEntry.type;
+  }
+};
+
+const isApprochedByUrl = () => {
+  return getBrowserNavigationType() === 'navigate';
+};
+
 const useMoimCreationInfo = () => {
   const location = useLocation();
   const navigationType = useNavigationType();
+
   const currentStep: MoimCreationStep = location.state?.step || steps[0];
 
   const isStartOfMoimCreation =
-    currentStep === '이름입력' && navigationType === 'PUSH';
+    currentStep === '이름입력' &&
+    (navigationType === 'PUSH' || isApprochedByUrl());
+
   if (isStartOfMoimCreation) {
     sessionStorage.removeItem('moimCreationInfo');
   }
