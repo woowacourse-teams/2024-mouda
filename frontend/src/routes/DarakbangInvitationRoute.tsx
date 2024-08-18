7cb85724176b9581609ddad4fe457941c4a0059f
ss0526100 <ss0526100@gmail.com>
2024-08-18 22:36:40 +0900

    feat(DarakbangInvitationRoute):코드로 다락방 이름 검사 구현

--- frontend/src/routes/DarakbangInvitationRoute.tsx
+++ frontend/src/routes/DarakbangInvitationRoute.tsx
@@ -4,14 +4,16 @@ import Button from '@_components/Button/Button';
 import CompleteLayout from '@_layouts/CompleteLayout/CompleteLayout';
 import MissingFallback from '@_components/MissingFallback/MissingFallback';
 import ROUTES from '@_constants/routes';
+import useDarakbangNameByCode from '@_hooks/queries/useDarakbangNameByCode';
 
 export default function DarakbangInvitationRoute() {
   const [searchParam] = useSearchParams();
   const navigate = useNavigate();
 
   const code = searchParam.get('code');
-  const isRightCode = code === '1';
-  const darakbangName = '우아한테크코스';
+  const { darakbangName, isLoading } = useDarakbangNameByCode(code || '');
+  const isRightCode = code !== '';
+  if (isLoading) return null;
   if (isRightCode) {
     navigate(ROUTES.darakbangNickname, { state: { darakbangName } });
   }
