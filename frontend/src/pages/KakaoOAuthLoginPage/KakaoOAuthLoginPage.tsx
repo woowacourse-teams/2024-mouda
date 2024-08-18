536dad68f2679064ca8a5afce377263f2370a814
ss0526100 <ss0526100@gmail.com>
2024-08-18 17:22:02 +0900

    feat:로그인 후 리다이렉션을 다락방선택페이지로 넘김

--- frontend/src/pages/KakaoOAuthLoginPage/KakaoOAuthLoginPage.tsx
+++ frontend/src/pages/KakaoOAuthLoginPage/KakaoOAuthLoginPage.tsx
@@ -1,10 +1,10 @@
-import { kakaoOAuth } from '@_apis/auth';
 import ROUTES from '@_constants/routes';
-import useServeToken from '@_hooks/mutaions/useServeToken';
+import { kakaoOAuth } from '@_apis/auth';
 import { requestPermission } from '@_service/notification';
 import { setToken } from '@_utils/tokenManager';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useServeToken from '@_hooks/mutaions/useServeToken';
 
 export default function KakaoOAuthLoginPage() {
   const navigate = useNavigate();
@@ -20,7 +20,7 @@ export default function KakaoOAuthLoginPage() {
       const response = await kakaoOAuth(code);
       setToken(response.data.accessToken);
       requestPermission(mutate);
-      navigate(ROUTES.main);
+      navigate(ROUTES.darakbangSelectOption);
     };
 
     loginKakaoOAuth();
