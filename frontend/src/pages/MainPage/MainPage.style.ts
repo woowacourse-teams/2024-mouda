3dca74c9207be277af808226a0a846851ea55069
ss0526100 <ss0526100@gmail.com>
2024-08-18 21:52:54 +0900

    feat(MainPage): 홈화면에서 다락방 관리/변경 페이지 구현

--- frontend/src/pages/MainPage/MainPage.style.ts
+++ frontend/src/pages/MainPage/MainPage.style.ts
@@ -4,3 +4,10 @@ export const headerButton = css`
   background: none;
   border: none;
 `;
+
+export const headerLeft = css`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  font-size: 100%;
+`;
