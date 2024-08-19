274de80132496b7c4df96bb44195f07909d547a4
ay-eonii <naayen@naver.com>
2024-08-19 21:50:52 +0900

    feat: 다락방의 모임, 다락방의 해주세요 검증 AOP 파라미터 순서 강제

--- backend/src/main/java/mouda/backend/common/RequiredDarakbangAspect.java
+++ backend/src/main/java/mouda/backend/common/RequiredDarakbangAspect.java
@@ -1,7 +1,12 @@
 package mouda.backend.common;
 
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -18,18 +23,33 @@ import mouda.backend.please.repository.PleaseRepository;
 @RequiredArgsConstructor
 public class RequiredDarakbangAspect {
 
+	private static final List<String> REQUIRED_MOIM_PARAMETER_NAMES = List.of("darakbangId", "moimId");
+	private static final List<String> REQUIRED_PLEASE_PARAMETER_NAMES = List.of("darakbangId", "pleaseId");
+
 	private final MoimRepository moimRepository;
 	private final PleaseRepository pleaseRepository;
 
 	@Before(value = "@annotation(RequiredDarakbangMoim) && args(darakbangId, moimId,..)")
-	public void validateDarakbangMoim(Long darakbangId, Long moimId) {
+	public void validateDarakbangMoim(JoinPoint joinPoint, Long darakbangId, Long moimId) {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		Parameter[] parameters = signature.getMethod().getParameters();
+		for (int i = 0; i < REQUIRED_MOIM_PARAMETER_NAMES.size(); i++) {
+			assert REQUIRED_MOIM_PARAMETER_NAMES.get(i).equals(parameters[i].getName())
+				: "올바른 파라미터 순서는 darakbangId, moimId 입니다.";
+		}
 		if (!moimRepository.existsByIdAndDarakbangId(moimId, darakbangId)) {
 			throw new MoimException(HttpStatus.NOT_FOUND, MoimErrorMessage.NOT_FOUND);
 		}
 	}
 
 	@Before(value = "@annotation(RequiredDarakbangPlease) && args(darakbangId, pleaseId,..)")
-	public void validateDarakbangPlease(Long darakbangId, Long pleaseId) {
+	public void validateDarakbangPlease(JoinPoint joinPoint, Long darakbangId, Long pleaseId) {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		Parameter[] parameters = signature.getMethod().getParameters();
+		for (int i = 0; i < REQUIRED_PLEASE_PARAMETER_NAMES.size(); i++) {
+			assert REQUIRED_PLEASE_PARAMETER_NAMES.get(i).equals(parameters[i].getName())
+				: "올바른 파라미터 순서는 darakbangId, pleaseId 입니다.";
+		}
 		if (!pleaseRepository.existsByIdAndDarakbangId(pleaseId, darakbangId)) {
 			throw new PleaseException(HttpStatus.NOT_FOUND, PleaseErrorMessage.NOT_FOUND);
 		}
