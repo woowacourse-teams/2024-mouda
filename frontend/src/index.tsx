4c619455e4091e28f08a6f13c423efd4520fd1f9
jaeml06 <jaeml0630@gmail.com>
2024-08-20 14:38:54 +0900

    chore: public 폴더 안의 정적 파일 빌드 폴더에 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -30,7 +30,7 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['http://43.202.67.25/'],
+  tracePropagationTargets: ['https://dev.mouda.site/'],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
