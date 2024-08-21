bf89e425fbb21882a367ffac94cdd51157471285
ss0526100 <ss0526100@gmail.com>
2024-08-21 19:24:23 +0900

    fix: 홈 화면 네비게이션에 홈 화면 확인하도록 수정

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -4,22 +4,22 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 import GET_ROUTES from '@_common/getRoutes';
 import NavigationBarItem from '@_components/NavigationBarItem/NavigationBarItem';
+import { common } from '@_common/common.style';
 import { useState } from 'react';
 import { useTheme } from '@emotion/react';
 
 export type Tab = '홈' | '채팅' | '해주세요' | '마이페이지';
 
-const tabRoutes: Record<Tab, string> = {
-  홈: GET_ROUTES.nowDarakbang.main(),
-  채팅: GET_ROUTES.nowDarakbang.chat(),
-  해주세요: GET_ROUTES.nowDarakbang.please(),
-  마이페이지: GET_ROUTES.nowDarakbang.myPage(),
-};
-
 export default function NavigationBar() {
   const theme = useTheme();
   const navigate = useNavigate();
   const location = useLocation();
+  const tabRoutes: Record<Tab, string> = {
+    홈: GET_ROUTES.nowDarakbang.main(),
+    채팅: GET_ROUTES.nowDarakbang.chat(),
+    해주세요: GET_ROUTES.nowDarakbang.please(),
+    마이페이지: GET_ROUTES.nowDarakbang.myPage(),
+  };
 
   const [currentTab, setCurrentTab] = useState<Tab>(
     Object.keys(tabRoutes).find(
@@ -34,7 +34,7 @@ export default function NavigationBar() {
 
   return (
     <nav css={S.navigationBarContainer({ theme })}>
-      <ul css={S.navigationBarList}>
+      <ul css={[S.navigationBarList, common.cursorPointer]}>
         {Object.keys(tabRoutes).map((tab) => (
           <NavigationBarItem
             key={tab}
