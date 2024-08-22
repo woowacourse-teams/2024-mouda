16f35509db28969f2bf125431280955e06c7bf7d
cys4585 <dev.youngsu@gmail.com>
2024-08-22 13:46:19 +0900

    feat: 각 스탭의 입력란에 자동 포커스 및 엔터로 다음 스탭 이동 기능 구현

--- frontend/src/components/Funnel/FunnelInput/FunnelInput.tsx
+++ frontend/src/components/Funnel/FunnelInput/FunnelInput.tsx
@@ -1,13 +1,16 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes, Ref } from 'react';
 import * as S from './FunnelInput.style';
 import { useTheme } from '@emotion/react';
 
 interface FunnelInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export default function FunnelInput(props: FunnelInputProps) {
+export default forwardRef(function FunnelInput(
+  props: FunnelInputProps,
+  ref?: Ref<HTMLInputElement>,
+) {
   const { ...rest } = props;
 
   const theme = useTheme();
 
-  return <input css={S.input({ theme })} {...rest} />;
-}
+  return <input ref={ref} css={S.input({ theme })} {...rest} />;
+});
