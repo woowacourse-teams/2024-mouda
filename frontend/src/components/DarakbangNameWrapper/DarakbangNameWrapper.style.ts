98d4b8bc93df0781b76d4e6243766154f2f8c29a
ss0526100 <ss0526100@gmail.com>
2024-08-22 18:16:02 +0900

    feat: 다락방 이름 래퍼의 최대 길이 조정

--- frontend/src/components/DarakbangNameWrapper/DarakbangNameWrapper.style.ts
+++ frontend/src/components/DarakbangNameWrapper/DarakbangNameWrapper.style.ts
@@ -3,7 +3,7 @@ import { SerializedStyles, css } from '@emotion/react';
 export const name = ({ font }: { font: string | SerializedStyles }) => css`
   ${font}
   overflow-x: hidden;
-  max-width: 50vw;
+  max-width: 30%;
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
