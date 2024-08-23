869ed5859a629c35640414a11f7b9f5e459b087c
cys4585 <dev.youngsu@gmail.com>
2024-08-23 11:45:01 +0900

    노티 알림 문제 해결

--- frontend/src/pages/NotificationPage/NotificationPage.style.ts
+++ frontend/src/pages/NotificationPage/NotificationPage.style.ts
@@ -1,6 +1,10 @@
+import { DISPLAY_MAX_WIDTH } from '@_constants/styles';
 import { css, Theme } from '@emotion/react';
 
 export const ModalContent = (props: { theme: Theme }) => css`
   ${props.theme.typography.s1}
-  margin: 5rem;
+  width: 100vw;
+  max-width: calc(${DISPLAY_MAX_WIDTH} * 0.8);
+  margin-bottom: 4rem;
+  padding-right: 60px;
 `;
