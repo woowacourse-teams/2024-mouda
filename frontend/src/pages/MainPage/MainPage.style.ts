ec1641de1ecc380579ed0af21aa54af6e1513175
cys4585 <dev.youngsu@gmail.com>
2024-08-23 10:06:52 +0900

    fix: 재수정 (max width)

--- frontend/src/pages/MainPage/MainPage.style.ts
+++ frontend/src/pages/MainPage/MainPage.style.ts
@@ -16,7 +16,7 @@ export const headerLeft = css`
 export const ModalContent = (props: { theme: Theme }) => css`
   ${props.theme.typography.s1}
   width: 100vw;
-  max-width: calc(${DISPLAY_MAX_WIDTH});
+  max-width: calc(${DISPLAY_MAX_WIDTH} * 0.8);
   margin-bottom: 4rem;
   padding-right: 60px;
 `;
