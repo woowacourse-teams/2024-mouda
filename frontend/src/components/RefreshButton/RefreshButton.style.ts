a00876b30857d521ca99e88fe73d50288347e615
jaeml06 <jaeml0630@gmail.com>
2024-09-01 00:07:49 +0900

    feat: 새로고침 버튼 구현

---
+++ frontend/src/components/RefreshButton/RefreshButton.style.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const noneBaackgroundButton = css`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
---
+++ frontend/src/components/RefreshButton/RefreshButton.tsx
@@ -0,0 +1,20 @@
+import { useQueryClient } from '@tanstack/react-query';
+import QUERY_KEYS from '@_constants/queryKeys';
+import { getLastDarakbangId } from '@_common/lastDarakbangManager';
+import Refresh from '@_common/assets/refresh.svg';
+import { noneBaackgroundButton } from './RefreshButton.style';
+export default function RefreshButton() {
+  const queryClient = useQueryClient();
+  return (
+    <button
+      css={noneBaackgroundButton}
+      onClick={() =>
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.darakbang, getLastDarakbangId()],
+        })
+      }
+    >
+      <Refresh />
+    </button>
+  );
+}
