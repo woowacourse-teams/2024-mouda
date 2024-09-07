5a89e5ca904795008eebd5058cdae7ce9f0baa2f
pricelees <priceelees@gmail.com>
2024-09-07 17:50:06 +0900

    refactor: 찜 상태 변경 로직 수정
    Co-authored-by: ay-eonii <naayen@naver.com>

--- backend/src/main/java/mouda/backend/moim/implement/writer/ZzimWriter.java
+++ backend/src/main/java/mouda/backend/moim/implement/writer/ZzimWriter.java
@@ -1,10 +1,40 @@
 package mouda.backend.moim.implement.writer;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import mouda.backend.darakbangmember.domain.DarakbangMember;
+import mouda.backend.moim.domain.Moim;
+import mouda.backend.moim.domain.Zzim;
+import mouda.backend.moim.implement.finder.MoimFinder;
+import mouda.backend.moim.implement.finder.ZzimFinder;
+import mouda.backend.moim.infrastructure.ZzimRepository;
 
 @Component
 @RequiredArgsConstructor
 public class ZzimWriter {
+
+	private final ZzimRepository zzimRepository;
+	private final ZzimFinder zzimFinder;
+
+	public void updateZzimStatus(Moim moim, DarakbangMember darakbangMember) {
+		zzimFinder.find(moim.getId(), darakbangMember).ifPresentOrElse(
+			this::delete,
+			() -> save(moim, darakbangMember)
+		);
+	}
+
+	public Zzim save(Moim moim, DarakbangMember darakbangMember) {
+		Zzim zzim = Zzim.builder()
+			.moim(moim)
+			.darakbangMember(darakbangMember)
+			.build();
+		return zzimRepository.save(zzim);
+	}
+
+	public void delete(Zzim zzim) {
+		zzimRepository.delete(zzim);
+	}
 }
