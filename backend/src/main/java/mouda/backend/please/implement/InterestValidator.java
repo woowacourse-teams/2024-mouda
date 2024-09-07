65afde533e64c1328580a3ba02f2203478865530
hoyeonyy <highoption@naver.com>
2024-09-08 01:42:24 +0900

    feat: InterestValidator 구현

---
+++ backend/src/main/java/mouda/backend/please/implement/InterestValidator.java
@@ -0,0 +1,22 @@
+package mouda.backend.please.implement;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import mouda.backend.darakbangmember.domain.DarakbangMember;
+import mouda.backend.please.domain.Please;
+import mouda.backend.please.infrastructure.InterestRepository;
+import mouda.backend.please.infrastructure.PleaseRepository;
+
+@Component
+@RequiredArgsConstructor
+public class InterestValidator {
+
+	private final InterestRepository interestRepository;
+	private final PleaseRepository pleaseRepository;
+	private final PleaseValidator pleaseValidator;
+
+	public void validate(Please please, Long darakbangId, boolean isInterested, DarakbangMember darakbangMember) {
+		pleaseValidator.validateNotInDarakbang(please, darakbangId);
+	}
+}
