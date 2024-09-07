5a7cd7b3b32e6cf03026a71489d7e89faa903e5a
pricelees <priceelees@gmail.com>
2024-09-07 16:38:24 +0900

    feat: implement 패키지 클래스 추가

---
+++ backend/src/main/java/mouda/backend/moim/implement/finder/ChatFinder.java
@@ -0,0 +1,10 @@
+package mouda.backend.moim.implement.finder;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ChatFinder {
+}
