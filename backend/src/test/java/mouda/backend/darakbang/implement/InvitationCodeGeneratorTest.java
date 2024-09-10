0deb26ae1638818c6333c859b69b63888ecb1503
김민겸 <mingyum119@naver.com>
2024-09-10 22:06:12 +0900

    test: 중복 참여 코드를 검증하는 테스트

--- backend/src/test/java/mouda/backend/darakbang/implement/InvitationCodeGeneratorTest.java
+++ backend/src/test/java/mouda/backend/darakbang/implement/InvitationCodeGeneratorTest.java
@@ -1,18 +1,26 @@
 package mouda.backend.darakbang.implement;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import mouda.backend.common.fixture.DarakbangFixture;
+import mouda.backend.darakbang.exception.DarakbangException;
+import mouda.backend.darakbang.infrastructure.DarakbangRepository;
 
 @SpringBootTest
 class InvitationCodeGeneratorTest {
 
+	@SpyBean
+	private InvitationCodeGenerator invitationCodeGenerator;
+
 	@Autowired
-	private
-	InvitationCodeGenerator invitationCodeGenerator;
+	private DarakbangRepository darakbangRepository;
 
 	@DisplayName("7자리 랜덤코드를 생성한다.")
 	@Test
@@ -20,5 +28,16 @@ class InvitationCodeGeneratorTest {
 		String invitationCode = invitationCodeGenerator.generate();
 
 		assertThat(invitationCode).hasSize(7);
+	}
+
+	@DisplayName("초대코드가 중복되면 랜덤 코드 생성에 실패한다.")
+	@Test
+	void failToCreateDarakbangWithDuplicatedCode() {
+		darakbangRepository.save(DarakbangFixture.getDarakbangWithWooteco());
+
+		doReturn("SOFABAC").when(invitationCodeGenerator).generate();
+
+		assertThatThrownBy(() -> invitationCodeGenerator.generate())
+			.isInstanceOf(DarakbangException.class);
 	}
 }
