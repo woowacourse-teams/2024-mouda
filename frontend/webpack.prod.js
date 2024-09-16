7f0cad1bbbc38d9521e31fd1a3dba370fcc1e00a
jaeml06 <jaeml0630@gmail.com>
2024-09-16 17:39:42 +0900

    Update webpack.prod.js

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -16,31 +16,6 @@ module.exports = merge(common, {
       },
     ],
   },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true, // 병렬 처리 활성화
-        terserOptions: {
-          compress: {
-            drop_console: true, // 콘솔 로그 제거
-          },
-        },
-      }),
-    ],
-    splitChunks: {
-      chunks: 'all', // 모든 타입의 청크를 분할
-      maxInitialRequests: 3,
-      maxAsyncRequests: 5,
-      minSize: 30000,
-    },
-    runtimeChunk: 'single', // 런타임 청크를 분리
-  },
   plugins: [
     // Put the Sentry Webpack plugin after all other plugins
     sentryWebpackPlugin({
@@ -48,6 +23,7 @@ module.exports = merge(common, {
       org: '2024-mouda',
       project: 'javascript-react',
     }),
+    
     new CopyWebpackPlugin({
       patterns: [
         { from: 'public', to: '' }, // public 폴더의 모든 파일을 dist 폴더의 루트로 복사
