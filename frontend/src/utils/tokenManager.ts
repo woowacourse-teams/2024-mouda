7f649b1c65311f1a5433eb0baaafaa03359661cf
jaeml06 <jaeml0630@gmail.com>
2024-09-24 00:27:44 +0900

    feat: msw환경에서 access token 모킹 추가

--- frontend/src/utils/tokenManager.ts
+++ frontend/src/utils/tokenManager.ts
@@ -1,13 +1,19 @@
 const TOKEN_KEY = 'access-token';
 
 export const setToken = (token: string): void => {
-  localStorage.setItem(TOKEN_KEY, token);
+  if (process.env.MSW !== 'true') {
+    localStorage.setItem(TOKEN_KEY, token);
+  }
 };
 
 // TODO: token 관리 로직 추가 필요(토큰 존재, 토큰 만료)
 export const getToken = () => {
-  const token = localStorage.getItem(TOKEN_KEY);
-  return token;
+  if (process.env.MSW !== 'true') {
+    const token = localStorage.getItem(TOKEN_KEY);
+    return token;
+  } else {
+    return 'random';
+  }
 };
 
 export const removeToken = (): void => {
