6ae166f02fb5224eb68ec1aa7ff9b30c4eae305e
김민겸 <mingyum119@naver.com>
2024-09-24 19:27:37 +0900

    refactor: 사용자 정보에 소셜 로그인 타입과 아이디를 함께 저장

---
+++ backend/src/main/java/mouda/backend/member/domain/LoginDetail.java
@@ -0,0 +1,19 @@
+package mouda.backend.member.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class LoginDetail {
+	private OauthType oauthType;
+	private Long socialLoinId;
+
+	protected LoginDetail() {
+	}
+
+	public LoginDetail(OauthType oauthType, Long socialLoinId) {
+		this.oauthType = oauthType;
+		this.socialLoinId = socialLoinId;
+	}
+}
