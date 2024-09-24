05ac2303bee47d228d49c218d7f62455cbaa6acb
jaeml06 <jaeml0630@gmail.com>
2024-09-24 10:56:54 +0900

    refactor: png 파일을 svg로 대체

--- frontend/src/components/UserPreview/UserPreview.style.ts
+++ frontend/src/components/UserPreview/UserPreview.style.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import defaultProfile from '@_common/assets/default_profile.png';
+import defaultProfile from '@_common/assets/empty_profile.svg?url';
 
 export const preview = ({
   imageUrl,
