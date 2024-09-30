b1cd273c319019d10ccd6b70a93a03418f199262
ss0526100 <ss0526100@gmail.com>
2024-09-30 13:07:36 +0900

    delete(formatters):removeBaseUrl 삭제

--- frontend/src/utils/formatters.ts
+++ frontend/src/utils/formatters.ts
@@ -61,7 +61,3 @@ export const formatHhmmToKoreanWithPrefix = (
 
   return `오전 ${hour}:${minute.toString().padStart(2, '00')}`;
 };
-
-export const removeBaseUrl = (redirectUrl: string) => {
-  return redirectUrl.replace(`${process.env.BASE_URL}/`, '');
-};
