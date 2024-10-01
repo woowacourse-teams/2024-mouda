5321ffb473dd91284a44fde6af50198db4cd777c
SungKyum Kim <sungkyum1@naver.com>
2024-10-01 14:52:13 +0900

    refactor: 참여자 조회 로직 분리

--- backend/src/main/java/mouda/backend/bet/implement/ParticipantFinder.java
+++ backend/src/main/java/mouda/backend/bet/implement/ParticipantFinder.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import mouda.backend.bet.domain.Participant;
-import mouda.backend.bet.entity.BetDarakbangMemberEntity;
 import mouda.backend.bet.entity.BetEntity;
 import mouda.backend.bet.infrastructure.BetDarakbangMemberRepository;
 import mouda.backend.darakbangmember.domain.DarakbangMember;
@@ -15,15 +14,13 @@ import mouda.backend.darakbangmember.domain.DarakbangMember;
 @RequiredArgsConstructor
 public class ParticipantFinder {
 
-	private final BetDarakbangMemberRepository betDarakbangMemberRepository;
+    private final BetDarakbangMemberRepository betDarakbangMemberRepository;
 
-	public List<Participant> findAllByBetEntity(BetEntity betEntity) {
-		List<BetDarakbangMemberEntity> betDarakbangMemberEntities = betDarakbangMemberRepository.findAllByBetId(betEntity.getId());
-		return betDarakbangMemberEntities.stream()
-			.map(betDarakbangMemberEntity -> {
-				DarakbangMember darakbangMember = betDarakbangMemberEntity.getDarakbangMember();
-				return new Participant(darakbangMember.getId(), darakbangMember.getNickname());
-			})
-			.toList();
-	}
+    public List<Participant> findAllByBetEntity(BetEntity betEntity) {
+        List<DarakbangMember> darakbangMembers = betDarakbangMemberRepository.findAllDarakbangMemberByBetId(
+            betEntity.getId());
+        return darakbangMembers.stream()
+            .map(darakbangMember -> new Participant(darakbangMember.getId(), darakbangMember.getNickname()))
+            .toList();
+    }
 }
