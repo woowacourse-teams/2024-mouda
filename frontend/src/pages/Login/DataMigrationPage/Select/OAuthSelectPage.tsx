bc91837d061203c28533054bfc7c965563b09bd5
jaeml06 <jaeml0630@gmail.com>
2024-10-03 14:05:18 +0900

    feat: 데이터 전환 로그인 선택 페이지 구현

---
+++ frontend/src/pages/Login/DataMigrationPage/Select/OAuthSelectPage.tsx
@@ -0,0 +1,80 @@
+import LoginLayout from '@_layouts/LoginLayout/LoginLayout';
+import MissingFallback from '@_components/MissingFallback/MissingFallback';
+import AppleOAuthIcon from '@_components/Icons/AppleOAuthIcon';
+import GoogleOAuthIcon from '@_common/assets/googleLogin.svg';
+export default function OAuthSelectPage() {
+  const appleAuthLogin = () => {
+    if (
+      !process.env.APPLE_O_AUTH_CLIENT_ID ||
+      !process.env.APPLE_OAUTH_REDIRECT_URI
+    ) {
+      throw new Error('Apple OAuth 정보가 없습니다.');
+    }
+
+    const params = {
+      client_id: process.env.APPLE_O_AUTH_CLIENT_ID,
+      redirect_uri: process.env.APPLE_OAUTH_REDIRECT_URI,
+      response_type: 'code',
+      scope: 'openid',
+    };
+    const queryString = new URLSearchParams(params).toString();
+    const appleOAuthUrl = `${process.env.APPLE_REQUEST_URL}?${queryString}`;
+    if (process.env.MSW == 'true') {
+      window.location.href = 'http://localhost:8081/kakao-o-auth?code=1';
+    } else {
+      window.location.href = appleOAuthUrl;
+    }
+  };
+
+  const goolgeAuthLogin = () => {
+    if (
+      !process.env.GOOGLE_O_AUTH_CLIENT_ID ||
+      !process.env.GOOGLE_OAUTH_REDIRECT_URI
+    ) {
+      throw new Error('Google OAuth 정보가 없습니다.');
+    }
+    const params = {
+      client_id: process.env.GOOGLE_O_AUTH_CLIENT_ID,
+      redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URI,
+      response_type: 'code',
+      scope: 'openid profile',
+    };
+    const queryString = new URLSearchParams(params).toString();
+    const googleOAuthUrl = `${process.env.GOOGLE_REQUEST_URL}?${queryString}`;
+    if (process.env.MSW == 'true') {
+      window.location.href = 'http://localhost:8081/kakao-o-auth?code=1';
+    } else {
+      window.location.href = googleOAuthUrl;
+    }
+  };
+  return (
+    <LoginLayout>
+      <LoginLayout.Header></LoginLayout.Header>
+      <LoginLayout.Main>
+        <section>
+          <MissingFallback text="앞으로 하실 로그인을 선택해주세요" />
+        </section>
+      </LoginLayout.Main>
+      <LoginLayout.Footer>
+        <button
+          css={{
+            background: 'none',
+            border: 'none',
+          }}
+          onClick={appleAuthLogin}
+        >
+          <AppleOAuthIcon />
+        </button>
+        <button
+          css={{
+            background: 'none',
+            border: 'none',
+          }}
+          onClick={goolgeAuthLogin}
+        >
+          <GoogleOAuthIcon />
+        </button>
+      </LoginLayout.Footer>
+    </LoginLayout>
+  );
+}
