6f594d1b6c53432728630588ab8140f73bcf7813
jaeml06 <jaeml0630@gmail.com>
2024-10-03 15:30:32 +0900

    feat: 서버에 memberId를 보낼 수 있도록 수정

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,29 +1,29 @@
 import ApiClient from './apiClient';
 
-export const login = async (loginInputInfo: { nickname: string }) => {
-  const response = await ApiClient.postWithoutAuth(
-    '/auth/login',
-    loginInputInfo,
-  );
-  return response.json();
-};
-
 export const kakaoOAuth = async (code: string) => {
   const response = await ApiClient.postWithoutAuth('/auth/kakao/oauth', {
     code,
   });
   return response.json();
 };
-export const appleOAuth = async (code: string) => {
+export const appleOAuth = async (
+  code: string,
+  memberId: string | null = null,
+) => {
   const response = await ApiClient.postWithoutAuth('/auth/apple/oauth', {
     code,
+    memberId,
   });
   return response.json();
 };
 
-export const googleOAuth = async (code: string) => {
+export const googleOAuth = async (
+  code: string,
+  memberId: string | null = null,
+) => {
   const response = await ApiClient.postWithoutAuth('/auth/google/oauth', {
     code,
+    memberId,
   });
   return response.json();
 };
