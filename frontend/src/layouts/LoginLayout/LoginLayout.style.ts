5087b09da49de38ec123e0dbd7fb8e7a67028461
jaeml06 <jaeml0630@gmail.com>
2024-10-03 13:31:39 +0900

    feat: LoginLayout 스타일 변경

--- frontend/src/layouts/LoginLayout/LoginLayout.style.ts
+++ frontend/src/layouts/LoginLayout/LoginLayout.style.ts
@@ -3,5 +3,6 @@ import { css } from '@emotion/react';
 export const layoutStyle = css`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   min-height: 100vh;
 `;
--- frontend/src/layouts/LoginLayout/LoginLayout.tsx
+++ frontend/src/layouts/LoginLayout/LoginLayout.tsx
@@ -1,10 +1,8 @@
 import { PropsWithChildren } from 'react';
 import * as S from './LoginLayout.style';
 import LoginMain from './LoginMain/LoginMain';
-// import LoginFooter from './LoginFooter/LoginFooter';
+import LoginFooter from './LoginFooter/LoginFooter';
 import StickyTriSectionHeader from '@_layouts/components/StickyTriSectionHeader/StickyTriSectionHeader';
-import InformationBottomWrapper from '@_layouts/InformationLayout/InformationBottomWrapper/InformationBottomWrapper';
-
 function LoginLayout(props: PropsWithChildren) {
   const { children } = props;
 
@@ -13,6 +11,6 @@ function LoginLayout(props: PropsWithChildren) {
 
 LoginLayout.Header = StickyTriSectionHeader;
 LoginLayout.Main = LoginMain;
-LoginLayout.Footer = InformationBottomWrapper;
+LoginLayout.Footer = LoginFooter;
 
 export default LoginLayout;
