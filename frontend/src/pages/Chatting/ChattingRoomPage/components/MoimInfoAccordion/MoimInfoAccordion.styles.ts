a89098d14bd495a01b827532508b7ea5a6bd252c
ss0526100 <ss0526100@gmail.com>
2024-10-03 20:52:09 +0900

    feat:모임정보아코디언 추가

---
+++ frontend/src/pages/Chatting/ChattingRoomPage/components/MoimInfoAccordion/MoimInfoAccordion.styles.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+export const accordion = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 7rem;
+  padding: 2rem;
+
+  border-radius: 0 0 1rem 1rem;
+  box-shadow: 0 4px 5px rgb(0 0 0 / 30%);
+`;
+
+export const textArea = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
---
+++ frontend/src/pages/Chatting/ChattingRoomPage/components/MoimInfoAccordion/MoimInfoAccordion.tsx
@@ -0,0 +1,37 @@
+import * as S from './MoimInfoAccordion.styles';
+
+import { MoimStatus } from '@_types/index';
+import Tag from '@_pages/Moim/MoimDetailPage/components/Tag/Tag';
+import { formatHhmmToKoreanWithPrefix } from '@_utils/formatters';
+import { useTheme } from '@emotion/react';
+
+interface MoimInfoAccordionProps {
+  date?: string;
+  time?: string;
+  place?: string;
+  status: MoimStatus;
+}
+
+export default function MoimInfoAccordion(props: MoimInfoAccordionProps) {
+  const { date, time, place, status } = props;
+  const theme = useTheme();
+  return (
+    <section css={S.accordion}>
+      <Tag status={status} />
+      <div
+        css={[
+          S.textArea,
+          theme.coloredTypography.Medium(theme.colorPalette.grey[500]),
+        ]}
+      >
+        <div
+          css={[theme.coloredTypography.Medium(theme.colorPalette.grey[500])]}
+        >
+          {date && date.replaceAll('-', '.')}
+          {time && ' ' + formatHhmmToKoreanWithPrefix(time)}
+        </div>
+        {place && place}
+      </div>
+    </section>
+  );
+}
