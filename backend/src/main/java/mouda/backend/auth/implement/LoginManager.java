84a4964424860f62bc483d00785d18a0cce38e55
hoyeonyy <highoption@naver.com>
2024-10-04 14:18:40 +0900

    feat: log 추가

--- backend/src/main/java/mouda/backend/auth/implement/LoginManager.java
+++ backend/src/main/java/mouda/backend/auth/implement/LoginManager.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mouda.backend.auth.implement.jwt.AccessTokenProvider;
 import mouda.backend.member.domain.LoginDetail;
 import mouda.backend.member.domain.Member;
@@ -14,6 +15,7 @@ import mouda.backend.member.infrastructure.MemberRepository;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LoginManager {
 
 	private final MemberRepository memberRepository;
@@ -22,17 +24,17 @@ public class LoginManager {
 
 	public String processSocialLogin(OauthType oauthType, String socialLoginId) {
 		Optional<Member> member = memberRepository.findByLoginDetail_SocialLoginId(socialLoginId);
-
+		log.error("1번");
 		if (member.isPresent()) {
 			return accessTokenProvider.provide(member.get());
 		}
-
+		log.error("2번");
 		Member newMember = Member.builder()
 			.nickname("nickname")
 			.loginDetail(new LoginDetail(oauthType, socialLoginId))
 			.build();
 		memberWriter.append(newMember);
-
+		log.error("3번");
 		return accessTokenProvider.provide(newMember);
 	}
 }
