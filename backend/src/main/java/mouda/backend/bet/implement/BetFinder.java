f248efa9b50f82ceb2190603f8ff1b89a89755ef
ay-eonii <naayen@naver.com>
2024-10-04 10:49:37 +0900

    fix: 안내면진다 조회시 당첨자 가져오도록 수정

--- backend/src/main/java/mouda/backend/bet/implement/BetFinder.java
+++ backend/src/main/java/mouda/backend/bet/implement/BetFinder.java
@@ -19,65 +19,66 @@ import mouda.backend.bet.infrastructure.BetRepository;
 @RequiredArgsConstructor
 public class BetFinder {
 
-    private final BetDarakbangMemberRepository betDarakbangMemberRepository;
-    private final BetRepository betRepository;
-    private final ParticipantFinder participantFinder;
+	private final BetDarakbangMemberRepository betDarakbangMemberRepository;
+	private final BetRepository betRepository;
+	private final ParticipantFinder participantFinder;
 
-    public Bet find(long darakbangId, long betEntityId) {
-        BetEntity betEntity = betRepository.findByIdAndDarakbangId(betEntityId, darakbangId)
-            .orElseThrow(IllegalArgumentException::new);
-        List<Participant> participants = participantFinder.findAllByBetEntity(betEntity);
+	public Bet find(long darakbangId, long betEntityId) {
+		BetEntity betEntity = betRepository.findByIdAndDarakbangId(betEntityId, darakbangId)
+			.orElseThrow(IllegalArgumentException::new);
+		List<Participant> participants = participantFinder.findAllByBetEntity(betEntity);
 
-        return Bet.builder()
-            .betDetails(betEntity.toBetDetails())
-            .moimerId(betEntity.getMoimerId())
-            .participants(participants)
-            .build();
-    }
+		return Bet.builder()
+			.betDetails(betEntity.toBetDetails())
+			.moimerId(betEntity.getMoimerId())
+			.participants(participants)
+			.loserId(betEntity.getLoserDarakbangMemberId())
+			.build();
+	}
 
-    public List<Bet> findAllByDarakbangId(long darakbangId) {
-        List<BetEntity> betEntities = betRepository.findAllByDarakbangId(darakbangId);
+	public List<Bet> findAllByDarakbangId(long darakbangId) {
+		List<BetEntity> betEntities = betRepository.findAllByDarakbangId(darakbangId);
 
-        return createBets(betEntities);
-    }
+		return createBets(betEntities);
+	}
 
-    public List<Bet> findAllDrawableBet() {
-        List<BetEntity> betEntities = betRepository.findAllByBettingTimeAndLoserDarakbangMemberIdIsNull(
-            LocalDateTime.now().withSecond(0).withNano(0));
+	public List<Bet> findAllDrawableBet() {
+		List<BetEntity> betEntities = betRepository.findAllByBettingTimeAndLoserDarakbangMemberIdIsNull(
+			LocalDateTime.now().withSecond(0).withNano(0));
 
-        return createBets(betEntities);
-    }
+		return createBets(betEntities);
+	}
 
-    private List<Bet> createBets(List<BetEntity> betEntities) {
-        return betEntities.stream()
-            .map(this::createBet).toList();
-    }
+	private List<Bet> createBets(List<BetEntity> betEntities) {
+		return betEntities.stream()
+			.map(this::createBet).toList();
+	}
 
-    private Bet createBet(BetEntity betEntity) {
-        List<Participant> participants = participantFinder.findAllByBetEntity(betEntity);
-        return Bet.builder()
-            .betDetails(betEntity.toBetDetails())
-            .moimerId(betEntity.getMoimerId())
-            .loserId(betEntity.getLoserDarakbangMemberId())
-            .participants(participants)
-            .build();
-    }
+	private Bet createBet(BetEntity betEntity) {
+		List<Participant> participants = participantFinder.findAllByBetEntity(betEntity);
+		return Bet.builder()
+			.betDetails(betEntity.toBetDetails())
+			.moimerId(betEntity.getMoimerId())
+			.loserId(betEntity.getLoserDarakbangMemberId())
+			.participants(participants)
+			.build();
+	}
 
-    @Transactional(readOnly = true)
-    public Loser findResult(long darakbangId, long betId) {
-        // TODO : 예외처리 공통화
-        BetEntity betEntity = betRepository.findByIdAndDarakbangId(betId, darakbangId)
-            .orElseThrow(IllegalArgumentException::new);
+	@Transactional(readOnly = true)
+	public Loser findResult(long darakbangId, long betId) {
+		// TODO : 예외처리 공통화
+		BetEntity betEntity = betRepository.findByIdAndDarakbangId(betId, darakbangId)
+			.orElseThrow(IllegalArgumentException::new);
 
-        // TODO : 리팩토링
-        Long loserDarakbangMemberId = betEntity.getLoserDarakbangMemberId();
-        if (loserDarakbangMemberId == null) {
-            throw new IllegalArgumentException("아직 추첨이 진행되지 않았습니다.");
-        }
+		// TODO : 리팩토링
+		Long loserDarakbangMemberId = betEntity.getLoserDarakbangMemberId();
+		if (loserDarakbangMemberId == null) {
+			throw new IllegalArgumentException("아직 추첨이 진행되지 않았습니다.");
+		}
 
-        BetDarakbangMemberEntity betDarakbangMemberEntity = betDarakbangMemberRepository.findByBetIdAndDarakbangMemberId(
-            betId, loserDarakbangMemberId).orElseThrow(IllegalArgumentException::new);
-        return new Loser(betDarakbangMemberEntity.getDarakbangMember().getId(),
-            betDarakbangMemberEntity.getDarakbangMember().getNickname());
-    }
+		BetDarakbangMemberEntity betDarakbangMemberEntity = betDarakbangMemberRepository.findByBetIdAndDarakbangMemberId(
+			betId, loserDarakbangMemberId).orElseThrow(IllegalArgumentException::new);
+		return new Loser(betDarakbangMemberEntity.getDarakbangMember().getId(),
+			betDarakbangMemberEntity.getDarakbangMember().getNickname());
+	}
 }
