102028efa5580b4a175248285111e070a03bdfa1
ay-eonii <naayen@naver.com>
2024-10-08 22:18:54 +0900

    feat: ChatRoomEntity에 targetId와 type으로 고유키 설정

--- backend/src/main/java/mouda/backend/chat/entity/ChatRoomEntity.java
+++ backend/src/main/java/mouda/backend/chat/entity/ChatRoomEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,10 +13,18 @@ import lombok.NoArgsConstructor;
 import mouda.backend.chat.domain.ChatRoomType;
 
 @Entity
-@Table(name = "chat_room")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+	name = "chat_room",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			columnNames = {"target_id", "type"}
+		)
+	}
+)
 public class ChatRoomEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
