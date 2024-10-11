3b2b9e2a883ae4ea66f27e1e84b444c6028b7a23
ay-eonii <naayen@naver.com>
2024-10-11 17:00:54 +0900

    feat: BetException 예외 적용

--- backend/src/main/java/mouda/backend/bet/exception/BetErrorMessage.java
+++ backend/src/main/java/mouda/backend/bet/exception/BetErrorMessage.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BetErrorMessage {
 
-	BET_DARAKBANG_MEMBER_NOT_FOUND("참여하지 않은 안내면진다입니다.");
-
+	BET_DARAKBANG_MEMBER_NOT_FOUND("참여하지 않은 안내면진다입니다."),
+	BET_NOT_FOUND("안내면진다가 존재하지 않습니다."),
+	LOSER_NOT_FOUND("당첨자가 존재하지 않습니다.");
+	
 	private final String message;
 }
