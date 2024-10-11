535669d77c063ad92dcde6447c375a60ed653447
SungKyum Kim <sungkyum1@naver.com>
2024-10-11 16:58:57 +0900

    feat: 채팅방 타입별 속성을 반환하기 위한 추상화 구현레이어 클래스

---
+++ backend/src/main/java/mouda/backend/chat/implement/AttributeManagerRegistry.java
@@ -0,0 +1,22 @@
+package mouda.backend.chat.implement;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import mouda.backend.chat.domain.ChatRoomType;
+
+@Component
+@RequiredArgsConstructor
+public class AttributeManagerRegistry {
+
+	private final List<AttributeManager> attributeManagers;
+
+	public AttributeManager getManager(ChatRoomType chatRoomType) {
+		return attributeManagers.stream()
+			.filter(attributeManager -> attributeManager.support(chatRoomType))
+			.findFirst()
+			.orElseThrow(IllegalArgumentException::new);
+	}
+}
