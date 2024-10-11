677debff63f7825b642f476e6096f8ac1d87bcf7
ss0526100 <ss0526100@gmail.com>
2024-10-11 14:05:03 +0900

    feat: 채팅 필터 태그 추가

---
+++ frontend/src/pages/Chatting/ChatPage/components/ChatFilterTag/ChatFilterTag.styles.ts
@@ -0,0 +1,29 @@
+import { Theme, css } from '@emotion/react';
+
+export const tag = ({
+  theme,
+  isChecked,
+}: {
+  theme: Theme;
+  isChecked: boolean;
+}) => css`
+  ${theme.typography.b4};
+  box-sizing: content-box;
+  width: 50px;
+  height: 25px;
+  padding: 2px 2px 0;
+
+  color: ${isChecked
+    ? theme.colorPalette.white[100]
+    : theme.colorPalette.grey[600]};
+  text-align: center;
+
+  background-color: ${isChecked
+    ? theme.colorPalette.orange[400]
+    : theme.colorPalette.white[100]};
+  border: ${isChecked
+      ? theme.colorPalette.white[100]
+      : theme.colorPalette.grey[300]}
+    1px solid;
+  border-radius: 10px;
+`;
