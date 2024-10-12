8157a78e2ab1e9e37af2b2500489d36d336082b7
SungKyum Kim <sungkyum1@naver.com>
2024-10-12 23:59:37 +0900

    feat: equals & hashcode 재정의

--- backend/src/main/java/mouda/backend/chat/domain/Participant.java
+++ backend/src/main/java/mouda/backend/chat/domain/Participant.java
@@ -1,10 +1,12 @@
 package mouda.backend.chat.domain;
 
+import java.util.Objects;
+
 import lombok.Getter;
 
 @Getter
 public class Participant {
-	
+
 	private final String nickname;
 	private final String profile;
 	private final String role;
@@ -13,5 +15,20 @@ public class Participant {
 		this.nickname = nickname;
 		this.profile = profile;
 		this.role = role;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Participant that = (Participant)o;
+		return Objects.equals(nickname, that.nickname) && Objects.equals(profile, that.profile) && Objects.equals(role, that.role);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nickname, profile, role);
 	}
 }
