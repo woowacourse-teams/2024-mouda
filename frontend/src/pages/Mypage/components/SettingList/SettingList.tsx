2f06dd6ef926a9b7a2a742ae48a5bf4d2ee8b7d0
jaeml06 <jaeml0630@gmail.com>
2024-10-13 13:24:01 +0900

    feat: 로그아웃, 회원탈퇴시 home으로 이동

--- frontend/src/pages/Mypage/components/SettingList/SettingList.tsx
+++ frontend/src/pages/Mypage/components/SettingList/SettingList.tsx
@@ -2,15 +2,25 @@ import { removeAccessToken } from '@_utils/tokenManager';
 import * as S from './SettingList.style';
 import SettingCard from '../SettingCard/SettingCard';
 import useDeleteMyInfo from '@_hooks/mutaions/useDeleteMyInfo';
+import { useNavigate } from 'react-router-dom';
+import ROUTES from '@_constants/routes';
 
 export default function SettingList() {
+  const navigate = useNavigate();
   const { mutate } = useDeleteMyInfo();
   const Settings = [
-    { title: '로그아웃', onClick: () => removeAccessToken() },
+    {
+      title: '로그아웃',
+      onClick: () => {
+        removeAccessToken();
+        navigate(ROUTES.home);
+      },
+    },
     {
       title: '회원탈퇴',
       onClick: () => {
         mutate();
+        navigate(ROUTES.home);
       },
     },
   ];
