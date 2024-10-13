464b295aa93fd247c29627e3aa4ecba84c4854ae
hoyeonyy <highoption@naver.com>
2024-10-13 17:12:14 +0900

    feat: Json String을 객체로 파싱하고 code를 담아 보내도록 수정

--- backend/src/main/java/mouda/backend/auth/presentation/controller/AppleAuthController.java
+++ backend/src/main/java/mouda/backend/auth/presentation/controller/AppleAuthController.java
@@ -1,5 +1,6 @@
 package mouda.backend.auth.presentation.controller;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
@@ -9,32 +10,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mouda.backend.auth.business.AppleAuthService;
 import mouda.backend.auth.util.TokenDecoder;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class AppleAuthController {
 
 	private AppleAuthService appleAuthService;
+	private final ObjectMapper objectMapper;
 
 	@PostMapping("/v1/oauth/apple")
 	public ResponseEntity<Void> test(
+		@RequestParam("code") String code,
 		@RequestParam("id_token") String id_token,
 		@RequestParam("user") String user
-	) {
-		String firstName = user.get("name").get("firstName").asText();
-		String lastName = user.get("name").get("lastName").asText();
+	) throws IOException {
+		AppleUserInfoRequest request = objectMapper.readValue(user, AppleUserInfoRequest.class);
+
+		String firstName = request.name().firstName();
+		String lastName = request.name().lastName();
 		Map<String, String> stringStringMap = TokenDecoder.parseIdToken(id_token);
 		for (String s : stringStringMap.keySet()) {
 			log.info("{} : {}", s, stringStringMap.get(s));
 		}
-		//
 		log.error("firstName : {}, lastNAme: {}", firstName, lastName);
 		appleAuthService.save(id_token, firstName, lastName);
 		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.add("Location", "https://dev.mouda.site/oauth/apple");
+		httpHeaders.add("Location", "https://dev.mouda.site/oauth/apple?code=" + code);
 		return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
 	}
 }
