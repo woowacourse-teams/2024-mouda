bcd810ba4b55ef0d84f3adc7cd28982c580d366d
hoyeonyy <highoption@naver.com>
2024-10-13 15:58:37 +0900

    refactor: 컨트롤러 수정

--- backend/src/main/java/mouda/backend/auth/presentation/controller/AppleAuthController.java
+++ backend/src/main/java/mouda/backend/auth/presentation/controller/AppleAuthController.java
@@ -1,24 +1,28 @@
 package mouda.backend.auth.presentation.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import lombok.extern.slf4j.Slf4j;
 import mouda.backend.auth.business.AppleAuthService;
 
 @RestController
+@Slf4j
 public class AppleAuthController {
 
 	private AppleAuthService appleAuthService;
 
 	@PostMapping("/v1/oauth/apple")
-	public void test(@RequestBody JsonNode jsonNode) {
-		String idToken = jsonNode.get("authorization").get("id_token").asText();
-		String firstName = jsonNode.get("user").get("name").get("firstName").asText();
-		String lastName = jsonNode.get("user").get("name").get("lastName").asText();
-
-		appleAuthService.save(idToken, firstName, lastName);
+	public void test(
+		@RequestParam("id_token") String id_token,
+		@RequestParam("user") JsonNode user
+	) {
+		String firstName = user.get("name").get("firstName").asText();
+		String lastName = user.get("name").get("lastName").asText();
+		log.error("firstName : {}, lastNAme: {}", firstName, lastName);
+		appleAuthService.save(id_token, firstName, lastName);
 	}
 }
