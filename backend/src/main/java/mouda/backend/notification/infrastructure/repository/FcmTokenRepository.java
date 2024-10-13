c14ee0c4c17756b2a359795795915a79d2cfe932
pricelees <priceelees@gmail.com>
2024-10-13 14:00:37 +0900

    style: 컨벤션 수정

--- backend/src/main/java/mouda/backend/notification/infrastructure/repository/FcmTokenRepository.java
+++ backend/src/main/java/mouda/backend/notification/infrastructure/repository/FcmTokenRepository.java
@@ -9,9 +9,9 @@ import mouda.backend.notification.infrastructure.entity.FcmTokenEntity;
 
 public interface FcmTokenRepository extends JpaRepository<FcmTokenEntity, Long> {
 
-    List<FcmTokenEntity> findAllByMemberId(long memberId);
+	List<FcmTokenEntity> findAllByMemberId(long memberId);
 
-    Optional<FcmTokenEntity> findByToken(String token);
+	Optional<FcmTokenEntity> findByToken(String token);
 
 	void deleteAllByTokenIn(List<String> unregisteredTokens);
 }
