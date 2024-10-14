528911c0ecff50562e5e0bc0ba8801434bbcae9e
SungKyum Kim <sungkyum1@naver.com>
2024-10-14 13:45:07 +0900

    refactor: Loser 도메인을 Participant 도메인을 재사용하도록 변경

--- backend/src/main/java/mouda/backend/chat/domain/BetAttributes.java
+++ backend/src/main/java/mouda/backend/chat/domain/BetAttributes.java
@@ -8,30 +8,30 @@ import lombok.Getter;
 @Getter
 public class BetAttributes implements Attributes {
 
-	private final String title;
-	private final Boolean isLoser;
-	private final Long betId;
-	private final Loser loser;
+    private final String title;
+    private final Boolean isLoser;
+    private final Long betId;
+    private final Participant loser;
 
-	public BetAttributes(String title, Boolean isLoser, Long betId, Loser loser) {
-		this.title = title;
-		this.isLoser = isLoser;
-		this.betId = betId;
-		this.loser = loser;
-	}
+    public BetAttributes(String title, Boolean isLoser, Long betId, Participant loser) {
+        this.title = title;
+        this.isLoser = isLoser;
+        this.betId = betId;
+        this.loser = loser;
+    }
 
-	@Override
-	public Map<String, Object> getAttributes() {
-		Map<String, Object> attributes = new HashMap<>();
-		attributes.put("title", title);
-		attributes.put("isLoser", isLoser);
-		attributes.put("betId", betId);
-		attributes.put("loser", Map.of(
-			"nickname", loser.getNickname(),
-			"profile", loser.getProfile(),
-			"role", loser.getRole()
-		));
-		return attributes;
-	}
+    @Override
+    public Map<String, Object> getAttributes() {
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("title", title);
+        attributes.put("isLoser", isLoser);
+        attributes.put("betId", betId);
+        attributes.put("loser", Map.of(
+            "nickname", loser.getNickname(),
+            "profile", loser.getProfile(),
+            "role", loser.getRole()
+        ));
+        return attributes;
+    }
 
 }
