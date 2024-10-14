d1d6c0a8e0ccdd10191e00f8338e56f98a165a80
ay-eonii <naayen@naver.com>
2024-10-14 16:38:36 +0900

    refactor: 예외 처리 통일

--- backend/src/main/java/mouda/backend/chat/exception/ChatErrorMessage.java
+++ backend/src/main/java/mouda/backend/chat/exception/ChatErrorMessage.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ChatErrorMessage {
 
 	CHATROOM_NOT_FOUND("존재하지 않는 채팅방입니다."),
+	BET_DARAKBANG_MEMBER_NOT_FOUND("참여하지 않은 안내면진다입니다."),
 	INVALID_CHATROOM_TYPE("잘못된 채팅 방 타입입니다."),
 	UNAUTHORIZED("권한이 없습니다.");
 
