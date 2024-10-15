134e5afe5cbb0b7a0f5977ee425eddb22de8711a
jaeml06 <jaeml0630@gmail.com>
2024-10-15 16:16:51 +0900

    feat: 사용자 전환에서 Apple로그인 변경

--- frontend/src/pages/Login/DataMigrationPage/Select/OAuthSelectPage.tsx
+++ frontend/src/pages/Login/DataMigrationPage/Select/OAuthSelectPage.tsx
@@ -25,13 +25,14 @@ export default function OAuthSelectPage() {
 
     const params = {
       client_id: process.env.APPLE_O_AUTH_CLIENT_ID,
-      redirect_uri: process.env.APPLE_OAUTH_REDIRECT_URI,
-      response_type: 'code',
-      scope: 'openid',
+      redirect_uri: 'https://api.dev.mouda.site/v1/oauth/apple',
+      response_type: 'code id_token',
+      response_mode: 'form_post',
+      scope: 'name email',
     };
     const queryString = new URLSearchParams(params).toString();
     const appleOAuthUrl = `${process.env.APPLE_REQUEST_URL}?${queryString}`;
-    if (process.env.MSW == 'true') {
+    if (process.env.MSW === 'true') {
       window.location.href = 'http://localhost:8081/kakao-o-auth?code=1';
     } else {
       window.location.href = appleOAuthUrl;
