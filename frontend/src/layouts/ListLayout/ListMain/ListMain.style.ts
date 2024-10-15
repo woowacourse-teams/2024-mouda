48900be286d54e47a598a7b638c1489d8087ed78
cys4585 <dev.youngsu@gmail.com>
2024-10-15 15:16:16 +0900

    fix: 안내면진다에도 반영

--- frontend/src/layouts/ListLayout/ListMain/ListMain.style.ts
+++
@@ -1,5 +0,0 @@
-import { css } from '@emotion/react';
-
-export const main = () => css`
-  padding: 16px 22px 80px;
-`;
--- frontend/src/layouts/ListLayout/ListMain/ListMain.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react';
-import * as S from './ListMain.style';
-
-export default function ListMain(props: PropsWithChildren) {
-  const { children } = props;
-
-  return <main css={S.main}>{children}</main>;
-}
