b02091400dc1d36154d84a44e5a92fa5df19f3c3
김민겸 <mingyum119@naver.com>
2024-10-16 16:18:06 +0900

    test: 마이페이지 수정 테스트 작성
    `@MockBean`을 Nested 내부에 작성하면 에러가 터져서 외부에 선언하였습니다.

--- backend/src/test/java/mouda/backend/darakbangmember/business/DarakbangMemberServiceTest.java
+++ backend/src/test/java/mouda/backend/darakbangmember/business/DarakbangMemberServiceTest.java
@@ -1,18 +1,25 @@
 package mouda.backend.darakbangmember.business;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 
 import mouda.backend.common.fixture.DarakbangMemberFixture;
 import mouda.backend.common.fixture.DarakbangSetUp;
 import mouda.backend.common.fixture.MemberFixture;
-import mouda.backend.darakbang.infrastructure.DarakbangRepository;
 import mouda.backend.darakbangmember.domain.DarakbangMember;
+import mouda.backend.darakbangmember.implement.S3Client;
 import mouda.backend.darakbangmember.infrastructure.DarakbangMemberRepository;
 import mouda.backend.darakbangmember.presentation.response.DarakbangMemberResponses;
 import mouda.backend.darakbangmember.presentation.response.DarakbangMemberRoleResponse;
@@ -30,10 +37,13 @@ class DarakbangMemberServiceTest extends DarakbangSetUp {
 	private DarakbangMemberRepository darakbangMemberRepository;
 
 	@Autowired
-	private DarakbangRepository darakbangRepository;
-
-	@Autowired
 	private DarakbangMemberService darakbangMemberService;
+
+	@MockBean
+	private S3Client s3Client;
+
+	@Value("${aws.s3.prefix}")
+	private String prefix;
 
 	@DisplayName("다락방 멤버 권한 조회 테스트")
 	@Nested
@@ -85,5 +95,66 @@ class DarakbangMemberServiceTest extends DarakbangSetUp {
 		assertThat(response.nickname()).isEqualTo("hogee");
 		assertThat(response.profile()).isEqualTo("profile");
 		assertThat(response.description()).isEqualTo("description");
+	}
+
+	@DisplayName("마이페이지 수정 테스트")
+	@Nested
+	class UpdateMyInfoTest {
+
+		private String nickname = "수정된 닉네임";
+		private String description = "수정된 소개";
+
+		@DisplayName("이미지 추가 없이 닉네임, 소개만 변경하는 경우 S3 통신 없이 DB값을 변경한다.")
+		@Test
+		void updateMyInfoWhenNoProfileUpdate() {
+			// when
+			darakbangMemberService.updateMyInfo(darakbangAnna, "false", null, nickname, description);
+
+			// then
+			Optional<DarakbangMember> annaOptional = darakbangMemberRepository.findById(darakbangAnna.getId());
+			assertThat(annaOptional.isPresent()).isTrue();
+			assertThat(annaOptional.get().getNickname()).isEqualTo(nickname);
+			assertThat(annaOptional.get().getDescription()).isEqualTo(description);
+
+			verify(s3Client, times(0)).uploadFile(any());
+		}
+
+		@DisplayName("이미지 추가와 더불어 닉네임, 소개를 변경하는 경우 S3 통신과 함께 DB 값을 변경한다.")
+		@Test
+		void updateMyInfoWithProfileUpdate() {
+			// given
+			MockMultipartFile file = new MockMultipartFile("file", "test-file.txt",
+				MediaType.TEXT_PLAIN_VALUE, "Hello, World!".getBytes());
+			when(s3Client.uploadFile(any())).thenReturn("https://s3url/uuid.png");
+
+			// when
+			darakbangMemberService.updateMyInfo(darakbangAnna, "false", file, nickname, description);
+
+			// then
+			Optional<DarakbangMember> annaOptional = darakbangMemberRepository.findById(darakbangAnna.getId());
+			assertThat(annaOptional.isPresent()).isTrue();
+			assertThat(annaOptional.get().getNickname()).isEqualTo(nickname);
+			assertThat(annaOptional.get().getDescription()).isEqualTo(description);
+			String expectedUrl = prefix + "uuid.png";
+			assertThat(annaOptional.get().getProfile()).isEqualTo(expectedUrl);
+
+			verify(s3Client, times(1)).uploadFile(any());
+		}
+
+		@DisplayName("기존 이미지로 변경하는 경우 S3 통신 없이 DB의 프로필 이미지 URL 값을 제거한다.")
+		@Test
+		void updateMyInfoWithBasicProfileUpdate() {
+			// when
+			darakbangMemberService.updateMyInfo(darakbangAnna, "true", null, nickname, description);
+
+			// then
+			Optional<DarakbangMember> annaOptional = darakbangMemberRepository.findById(darakbangAnna.getId());
+			assertThat(annaOptional.isPresent()).isTrue();
+			assertThat(annaOptional.get().getNickname()).isEqualTo(nickname);
+			assertThat(annaOptional.get().getDescription()).isEqualTo(description);
+			assertThat(annaOptional.get().getProfile()).isNull();
+
+			verify(s3Client, times(0)).uploadFile(any());
+		}
 	}
 }
