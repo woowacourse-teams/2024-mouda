05e058a729073182125263892a14403e3e0ffdc6
jaeml06 <jaeml0630@gmail.com>
2024-10-17 14:02:34 +0900

    feat: input 에러 메세지 출력을 위한 LabeledInput 변경

--- frontend/src/pages/Mypage/components/MineInfoCard/MineInfoCard.tsx
+++ frontend/src/pages/Mypage/components/MineInfoCard/MineInfoCard.tsx
@@ -2,6 +2,9 @@ import * as S from './MineInfoCard.style';
 import ProfileFrame from '@_components/ProfileFrame/ProfileFrame';
 import { useTheme } from '@emotion/react';
 import Edit from '@_common/assets/edit.svg?url';
+import LabeledInput from '@_components/Input/MoimInput';
+import { ChangeEvent } from 'react';
+import { validateNickName } from '@_pages/Mypage/validate';
 
 interface MineInfoCardProps {
   myInfo: {
@@ -33,10 +36,14 @@ export default function MineInfoCard({
         {isEditing && <img src={Edit} alt="Edit" css={S.editSVG} />}
       </div>
       {isEditing ? (
-        <input
+        <LabeledInput
           css={S.input({ theme })}
           value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNickname(e.target.value)
+          }
+          placeholder="닉네임을 1자에서 12자이하로 지어주세요"
+          validateFun={validateNickName}
         />
       ) : (
         <span css={theme.typography.h5}>{nickname}</span>
