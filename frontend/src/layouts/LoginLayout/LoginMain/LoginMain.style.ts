9dfcf0810e2d5c1659f2312eab110e9badb60843
jaeml06 <jaeml0630@gmail.com>
2024-10-18 14:57:57 +0900

    feat: 로그인 페이지 레이아웃 css 변경

--- frontend/src/layouts/LoginLayout/LoginMain/LoginMain.style.ts
+++ frontend/src/layouts/LoginLayout/LoginMain/LoginMain.style.ts
@@ -3,9 +3,10 @@ import { css } from '@emotion/react';
 export const mainStyle = css`
   display: flex;
   flex-direction: column;
+  gap: 3rem;
   align-items: center;
   justify-content: center;
-
   height: 100%;
+  width: 100%;
   margin: 0 auto;
 `;
