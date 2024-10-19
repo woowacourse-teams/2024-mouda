a2d23e39326fc0007d23540f3e8faf9299aeb7de
김민겸 <mingyum119@naver.com>
2024-10-19 19:32:52 +0900

    refactor: 애플 서버로부터 회원 정보를 전달받는 API를 AuthController로 이동

--- backend/src/main/java/mouda/backend/auth/presentation/controller/AuthController.java
+++ backend/src/main/java/mouda/backend/auth/presentation/controller/AuthController.java
@@ -1,9 +1,12 @@
 package mouda.backend.auth.presentation.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -11,7 +14,6 @@ import mouda.backend.auth.business.AppleAuthService;
 import mouda.backend.auth.business.GoogleAuthService;
 import mouda.backend.auth.business.KakaoAuthService;
 import mouda.backend.auth.presentation.controller.swagger.AuthSwagger;
-import mouda.backend.auth.presentation.request.AppleOauthRequest;
 import mouda.backend.auth.presentation.request.GoogleOauthRequest;
 import mouda.backend.auth.presentation.request.OauthRequest;
 import mouda.backend.auth.presentation.response.LoginResponse;
@@ -23,10 +25,9 @@ import mouda.backend.common.response.RestResponse;
 public class AuthController implements AuthSwagger {
 
 	private final KakaoAuthService kakaoAuthService;
-	private final AppleAuthService appleAuthService;
 	private final GoogleAuthService googleAuthService;
+	private final AppleAuthService appleAuthService;
 
-	@Override
 	@PostMapping("/kakao")
 	public ResponseEntity<RestResponse<LoginResponse>> loginKakaoOauth(@RequestBody OauthRequest oauthRequest) {
 		LoginResponse response = kakaoAuthService.oauthLogin(oauthRequest);
@@ -34,7 +35,6 @@ public class AuthController implements AuthSwagger {
 		return ResponseEntity.ok().body(new RestResponse<>(response));
 	}
 
-	@Override
 	@PostMapping("/google")
 	public ResponseEntity<RestResponse<LoginResponse>> loginGoogleOauth(
 		@RequestBody GoogleOauthRequest googleOauthRequest) {
@@ -43,11 +43,18 @@ public class AuthController implements AuthSwagger {
 		return ResponseEntity.ok().body(new RestResponse<>(response));
 	}
 
-	@Override
-	@PostMapping("/apple/oauth")
-	public ResponseEntity<RestResponse<LoginResponse>> loginAppleOauth(@RequestBody AppleOauthRequest oauthRequest) {
-		LoginResponse response = appleAuthService.oauthLogin(oauthRequest);
-
-		return ResponseEntity.ok().body(new RestResponse<>(response));
+	@PostMapping("/apple")
+	public ResponseEntity<Void> loginAppleOauth(
+		@RequestParam("id_token") String id_token,
+		@RequestParam(name = "user", required = false) String user
+	) {
+		// TODO: 이전에 가입한 적 있지만 DB를 갈아엎어서 user가 들어오지 않는 경우 save에 실패한다.
+		if (user != null) {
+			appleAuthService.save(id_token, user);
+		}
+		String accessToken = appleAuthService.login(id_token);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("Location", "https://dev.mouda.site/oauth/apple?token=" + accessToken);
+		return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
 	}
 }
