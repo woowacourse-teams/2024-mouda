ac98c873d53f92cc8b5742a6981ec9f703a1cf02
pricelees <priceelees@gmail.com>
2024-10-20 17:58:13 +0900

    refactor: ChatDateTimeFormatter 세분화

--- backend/src/main/java/mouda/backend/chat/util/ChatDateTimeFormatter.java
+++ backend/src/main/java/mouda/backend/chat/util/ChatDateTimeFormatter.java
@@ -1,19 +1,47 @@
 package mouda.backend.chat.util;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.springframework.http.HttpStatus;
+
+import lombok.NoArgsConstructor;
+import mouda.backend.chat.exception.ChatErrorMessage;
+import mouda.backend.chat.exception.ChatException;
+
+/**
+ * 채팅방에서 날짜와 시간이 확정될 때 알림 메시지에 사용되는 날짜 / 시간 형식 변환을 위한 유틸리티 클래스
+ */
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class ChatDateTimeFormatter {
 
-	// 2024-10-21 17:08 형식의 데이터를 10월 21일 17시 08분으로 변경
-	public static String formatDateTime(String dateTime) {
-		String[] splitDateTime = dateTime.split(" ");
-		LocalDate date = LocalDate.parse(splitDateTime[0]);
-		LocalTime time = LocalTime.parse(splitDateTime[1]);
+	private static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm";
 
-		return date.getMonthValue() + "월 "
-			+ date.getDayOfMonth() + "일 "
-			+ time.getHour() + "시 "
-			+ time.getMinute() + "분";
+	public static String formatDateTime(String dateTime) {
+		LocalDateTime localDateTime = parseDateTime(dateTime);
+		StringBuilder formattedDateTime = new StringBuilder();
+
+		int year = localDateTime.getYear();
+		if (year > LocalDate.now().getYear()) {
+			formattedDateTime.append(year).append("년 ");
+		}
+
+		return formattedDateTime
+			.append(localDateTime.getMonthValue()).append("월 ")
+			.append(localDateTime.getDayOfMonth()).append("일 ")
+			.append(localDateTime.getHour()).append("시 ")
+			.append(localDateTime.getMinute()).append("분")
+			.toString();
+	}
+
+	private static LocalDateTime parseDateTime(String dateTime) {
+		try {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DEFAULT_DATETIME_FORMAT);
+			return LocalDateTime.parse(dateTime, formatter);
+		} catch (DateTimeException e) {
+			throw new ChatException(HttpStatus.BAD_REQUEST, ChatErrorMessage.INVALID_DATE_TIME_FORMAT);
+		}
 	}
 }
