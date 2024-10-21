182780cfc647d8d4d97b105fc0556061b2e41ee4
pricelees <priceelees@gmail.com>
2024-10-21 15:46:03 +0900

    refactor: 404 에러 토큰 제거를 Sender가 아니라 Handler에서 마지막에 처리하도록 수정 및 FcmFailedResponse에서의 변경 사항 반영

--- backend/src/main/java/mouda/backend/notification/implement/fcm/token/FcmTokenFinder.java
+++ backend/src/main/java/mouda/backend/notification/implement/fcm/token/FcmTokenFinder.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import mouda.backend.notification.domain.FcmToken;
 import mouda.backend.notification.domain.Recipient;
 import mouda.backend.notification.infrastructure.entity.FcmTokenEntity;
 import mouda.backend.notification.infrastructure.repository.FcmTokenRepository;
@@ -25,5 +26,15 @@ public class FcmTokenFinder {
 			fcmTokens.addAll(tokens);
 		}
 		return fcmTokens;
+	}
+
+	public List<FcmToken> readAllByTokensIn(List<String> tokens) {
+		return fcmTokenRepository.findAllByTokenIn(tokens).stream()
+			.map(entity -> FcmToken.builder()
+				.memberId(entity.getMemberId())
+				.token(entity.getToken())
+				.build()
+			)
+			.toList();
 	}
 }
