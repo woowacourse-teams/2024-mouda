8c7c3f5ae854b3559275a30dfa236014172b2c7c
jaeml06 <jaeml0630@gmail.com>
2024-10-22 10:58:14 +0900

    feat: 페이지의 title baseline을 맞추기 위해 css 변경

--- frontend/src/layouts/LoginLayout/LoginMain/LoginMain.style.ts
+++ frontend/src/layouts/LoginLayout/LoginMain/LoginMain.style.ts
@@ -2,10 +2,11 @@ import { css } from '@emotion/react';
 
 export const mainStyle = css`
   display: flex;
+  flex-grow: 1;
   flex-direction: column;
   gap: 3rem;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   height: 100%;
   width: 100%;
   margin: 0 auto;
