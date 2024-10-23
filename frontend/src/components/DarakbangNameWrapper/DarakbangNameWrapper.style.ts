d24e407b9cf501e71a3c7808df262323a068879e
ss0526100 <ss0526100@gmail.com>
2024-10-23 13:55:15 +0900

    style:eslint 적용

--- frontend/src/components/DarakbangNameWrapper/DarakbangNameWrapper.style.ts
+++ frontend/src/components/DarakbangNameWrapper/DarakbangNameWrapper.style.ts
@@ -2,9 +2,7 @@ import { SerializedStyles, css } from '@emotion/react';
 
 export const name = ({ font }: { font: string | SerializedStyles }) => css`
   ${font}
-
   max-width: 40vw;
-
   overflow-x: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
