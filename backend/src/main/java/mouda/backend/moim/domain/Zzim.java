80e6f53d5b556c1779ca8abe4d0ce01b3bd71b98
pricelees <priceelees@gmail.com>
2024-10-23 11:04:13 +0900

    refactor: 모임 도메인 테이블 이름 지정

--- backend/src/main/java/mouda/backend/moim/domain/Zzim.java
+++ backend/src/main/java/mouda/backend/moim/domain/Zzim.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import mouda.backend.darakbangmember.domain.DarakbangMember;
 
 @Entity
+@Table(name = "zzim")
 @Getter
 @NoArgsConstructor
 public class Zzim {
