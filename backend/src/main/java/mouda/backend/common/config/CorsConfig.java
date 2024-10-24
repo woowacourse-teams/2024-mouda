5406186c495fc64b2b4c6cfb2d37cd63959cd7ab
SungKyum Kim <sungkyum1@naver.com>
2024-10-24 19:14:40 +0900

    fix: cors 도메인 추가

--- backend/src/main/java/mouda/backend/common/config/CorsConfig.java
+++ backend/src/main/java/mouda/backend/common/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
 				"https://dev.mouda.site",
 				"https://appleid.apple.com",
 				"https://test.mouda.site", "http://test.mouda.site",
-				"https://mouda.site", "http://mouda.site"
+				"https://mouda.site", "http://mouda.site",
+				"https://www.mouda.site", "http://www.mouda.site"
 			)
 			.allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("Authorization", "Content-Type")
