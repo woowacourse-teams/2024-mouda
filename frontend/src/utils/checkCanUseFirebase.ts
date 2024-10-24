07a027ee76c7ecaa2778f82d14b5c4b022843487
jaeml06 <jaeml0630@gmail.com>
2024-10-24 20:48:00 +0900

    fix:알림 지원 환경인지 확인 로직 추가

--- frontend/src/utils/checkCanUseFirebase.ts
+++ frontend/src/utils/checkCanUseFirebase.ts
@@ -1,6 +1,12 @@
-export default function checkCanUseFirebase() {
+import { isSupported } from 'firebase/messaging';
+export default async function checkCanUseFirebase() {
   if (location.hostname === 'localhost') return true;
   if (location.protocol !== 'https:') return false;
   if (navigator.userAgent.indexOf('KAKAO') >= 0) return false;
+  const messagingSupported = await isSupported();
+  if (!messagingSupported) {
+    console.error("This browser doesn't support Firebase Messaging.");
+    return false;
+  }
   return true;
 }
