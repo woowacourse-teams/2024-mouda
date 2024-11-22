68d4caf3a346a20c7e3eb29ebac4ae7c1a1d4b23
jaeml06 <jaeml0630@gmail.com>
2024-11-22 16:32:51 +0900

    refactor: 불필요한 주석 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,12 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import createQueryClient from './queryClient';
 import fonts from '@_common/font.style';
-// import { removeInviteCode } from '@_common/inviteCodeManager';
 import reset from './common/reset.style';
 import router from '@_routes/router';
 import { theme } from '@_common/theme/theme.style';
 
 export default function App() {
   const queryClient = useMemo(createQueryClient, []);
-
-  // useEffect(() => {
-  //   window.addEventListener('beforeunload', removeInviteCode);
-  //   return () => {
-  //     window.removeEventListener('beforeunload', removeInviteCode);
-  //   };
-  // }, []);
 
   return (
     <>
